b5dfd9cd9307a46e569ab263f08f1841e6c91d24
HumabHatterZed <69230920+HumabHatterZed@users.noreply.github.com>
2022-05-17 13:23:13 -0700

    Update WstlUtils.cs

--- Core/Utils/WstlUtils.cs
+++ Core/Utils/WstlUtils.cs
@@ -8,6 +8,17 @@ using UnityEngine;
 
 namespace WhistleWindLobotomyMod
 {
+    public static class ImageUtils
+    {
+        public static Texture2D LoadTextureFromResource(byte[] resourceFile)
+        {
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage(resourceFile);
+            texture.filterMode = FilterMode.Point;
+            return texture;
+        }
+    }
+    
     public static class WstlUtils
     {
         // Originally taken from GrimoraMod and SigilADay_julienperge
