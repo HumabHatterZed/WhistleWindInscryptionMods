ab41df1f1603132cdae8c9ab60b188133aa5e531
Layton Gilbert <humanhaterq@gmail.com>
2022-07-26 15:27:44 -0700

    Changed rulebook judgebird description

--- Abilities/Rulebook/_rulebookJudgementBird.cs
+++ Abilities/Rulebook/_rulebookJudgementBird.cs
@@ -11,7 +11,7 @@ namespace WhistleWindLobotomyMod
         private void Rulebook_JudgementBird()
         {
             const string rulebookName = "Judgement Bird";
-            const string rulebookDescription = "Kills selected cards. This does not activate abilities like Sharp Quills.";
+            const string rulebookDescription = "Instantly kills cards it attacks, regardless of ability.";
             const string dialogue = "femboy";
             EntryJudgementBird.ability = AbilityHelper.CreateAbility<EntryJudgementBird>(
                 Resources.sigilAbnormality, Resources.sigilAbnormality_pixel,
