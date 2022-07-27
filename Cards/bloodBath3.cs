ed98c59270487d3e8795bec97ebaca0bb525c17f
Layton Gilbert <humanhaterq@gmail.com>
2022-07-27 10:23:57 -0700

    Nerfed Bloodbath 3 Power (3) --> (1)

--- Cards/bloodBath3.cs
+++ Cards/bloodBath3.cs
@@ -22,7 +22,7 @@ namespace WhistleWindLobotomyMod
             CardHelper.CreateCard(
                 "wstl_bloodBath3", "Bloodbath",
                 "A tub of blood. The hands of people you once loved wait inside.",
-                3, 3, 3, 0,
+                1, 3, 3, 0,
                 Resources.bloodBath3, Resources.bloodBath3_emission,
                 abilities: abilities, specialAbilities: specialAbilities, statIcon: SpecialStatIcon.SacrificesThisTurn,
                 metaCategories: new(), tribes: new(), traits: new());
