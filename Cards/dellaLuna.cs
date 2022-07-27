aa3d607c52d7e1befe7d88c636a6b90a87cd4949
Layton Gilbert <humanhaterq@gmail.com>
2022-07-26 21:01:41 -0700

    Della Luna Power 1 --> 2

--- Cards/dellaLuna.cs
+++ Cards/dellaLuna.cs
@@ -18,7 +18,7 @@ namespace WhistleWindLobotomyMod
             CardHelper.CreateCard(
                 "wstl_dellaLuna", "Il Pianto della Luna",
                 "Tales say that [c:bR]the moon[c:] bewitches man. In reality man despairs at it.",
-                1, 6, 3, 0,
+                2, 6, 3, 0,
                 Resources.dellaLuna, Resources.dellaLuna_emission,
                 abilities: abilities, specialAbilities: new(),
                 metaCategories: new(), tribes: new(), traits: new(),
