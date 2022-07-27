0cd34e9f3de815fd94026f7d0f6bcb27bf5acb12
Layton Gilbert <humanhaterq@gmail.com>
2022-07-26 23:57:36 -0700

    Added CardLoaderPatch for removing Backward Clock after use

---
+++ Patches/CardLoaderPatch.cs
@@ -0,0 +1,26 @@
+﻿using InscryptionAPI;
+using InscryptionAPI.Card;
+using DiskCardGame;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WhistleWindLobotomyMod
+{
+    [HarmonyPatch(typeof(CardLoader))]
+    public static class CardLoaderPatch
+    {
+        // Controls custom emission rules for added cards
+        // E.g., forced emissions (always glowy), custom colours
+        [HarmonyPostfix, HarmonyPatch(nameof(CardLoader.GetUnlockedCards))]
+        public static void RemoveBackWardClock(ref List<CardInfo> __result)
+        {
+            if (PersistentValues.HasUsedBackwardClock)
+            {
+                __result.RemoveAll((CardInfo x) => x.name == "wstl_backwardClock");
+            }
+        }
+    }
+}
