48ef7331d0865a57f4f5902870c27e68a53ff610
Layton Gilbert <humanhaterq@gmail.com>
2022-07-26 23:31:55 -0700

    Renamed NodePatcher to NodeSequencerPatches and made the patches actually work

---
+++ Patches/NodeSequencerPatches.cs
@@ -0,0 +1,56 @@
+﻿using InscryptionAPI;
+using InscryptionAPI.Card;
+using DiskCardGame;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WhistleWindLobotomyMod
+{
+    [HarmonyPatch(typeof(CardStatBoostSequencer))]
+    public class StatBoostPatcher
+    {
+        // Removes cards from valid pool of hosts for stat boosts
+        [HarmonyPostfix, HarmonyPatch(nameof(CardStatBoostSequencer.GetValidCards))]
+        public static void RemoveFromValidCards(ref List<CardInfo> __result)
+        {
+            __result.RemoveAll((CardInfo x) => x.SpecialAbilities.Contains(NothingThere.specialAbility)
+            || x.Abilities.Contains(TheTrain.ability)
+            || x.Abilities.Contains(TimeMachine.ability));
+        }
+    }
+    [HarmonyPatch(typeof(DuplicateMergeSequencer))]
+    public class DuplicateMergePatch
+    {
+        // Removes cards from valid pool of duplicate choices
+        [HarmonyPostfix, HarmonyPatch(nameof(DuplicateMergeSequencer.GetValidDuplicateCards))]
+        public static void RemoveFromValidCardsForDuplication(ref List<CardInfo> __result)
+        {
+            __result.RemoveAll((CardInfo x) => x.SpecialAbilities.Contains(NothingThere.specialAbility)
+            || x.Abilities.Contains(TheTrain.ability)
+            || x.Abilities.Contains(TimeMachine.ability));
+        }
+    }
+    [HarmonyPatch(typeof(CardMergeSequencer))]
+    public class CardMergePatch
+    {
+        // Removes cards from valid pool of hosts for card merges
+        [HarmonyPostfix, HarmonyPatch(nameof(CardMergeSequencer.GetValidCardsForHost))]
+        public static void RemoveFromValidCardsForHost(ref List<CardInfo> __result)
+        {
+            __result.RemoveAll((CardInfo x) => x.SpecialAbilities.Contains(NothingThere.specialAbility)
+            || x.Abilities.Contains(TheTrain.ability)
+            || x.Abilities.Contains(TimeMachine.ability));
+        }
+        // Removes cards from valid pool of sacrifices for card merges
+        [HarmonyPostfix, HarmonyPatch(nameof(CardMergeSequencer.GetValidCardsForSacrifice))]
+        public static void RemoveFromValidCardsForSacrifice(ref List<CardInfo> __result)
+        {
+            __result.RemoveAll((CardInfo x) => x.SpecialAbilities.Contains(NothingThere.specialAbility)
+            || x.Abilities.Contains(TheTrain.ability)
+            || x.Abilities.Contains(TimeMachine.ability));
+        }
+    }
+}
