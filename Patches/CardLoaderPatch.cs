5cbba73ee9448432b93f97b56c114d8ff5d719ed
Layton Gilbert <humanhaterq@gmail.com>
2022-07-28 20:18:28 -0700

    CardLoaderPatch now removes the three birds if the player has Apocalypse

--- Patches/CardLoaderPatch.cs
+++ Patches/CardLoaderPatch.cs
@@ -12,14 +12,18 @@ namespace WhistleWindLobotomyMod
     [HarmonyPatch(typeof(CardLoader))]
     public static class CardLoaderPatch
     {
-        // Controls custom emission rules for added cards
-        // E.g., forced emissions (always glowy), custom colours
+        // Removes select cards following specific sequences
         [HarmonyPostfix, HarmonyPatch(nameof(CardLoader.GetUnlockedCards))]
-        public static void RemoveBackWardClock(ref List<CardInfo> __result)
+        public static void RemoveUniqueCards(ref List<CardInfo> __result)
         {
             if (PersistentValues.HasUsedBackwardClock)
             {
                 __result.RemoveAll((CardInfo x) => x.name == "wstl_backwardClock");
+            }
+            if (PersistentValues.HasApocalypse)
+            {
+                __result.RemoveAll((CardInfo x) => x.name == "wstl_punishingBird"
+                || x.name == "wstl_bigBird" || x.name == "wstl_judgementBird");
             }
         }
     }
