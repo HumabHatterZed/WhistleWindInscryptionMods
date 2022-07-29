bff054c22cd1feeb228e50717909d512d5137326
Layton Gilbert <humanhaterq@gmail.com>
2022-07-28 20:25:57 -0700

    Added patch to reset table effects of Apocalypse Bird

---
+++ Patches/OpponentPatch.cs
@@ -0,0 +1,27 @@
+﻿using InscryptionAPI;
+using InscryptionAPI.Card;
+using DiskCardGame;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WhistleWindLobotomyMod
+{
+    [HarmonyPatch(typeof(Opponent))]
+    public class OpponentPatch
+    {
+        // Adds Nothing There to the deck when chosen in a card choice (Trader, Boss Box, etc.)
+        [HarmonyPostfix, HarmonyPatch(nameof(Opponent.OutroSequence))]
+        public static IEnumerator ResetEffects(IEnumerator enumerator)
+        {
+            if (PersistentValues.HasSeenApocalypseEffects)
+            {
+                PersistentValues.HasSeenApocalypseEffects = false;
+                Singleton<TableVisualEffectsManager>.Instance.ResetTableColors();
+            }
+            yield return enumerator;
+        }
+    }
+}
