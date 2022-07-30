371cbfb50d85f91b4c0c3fb8ab23a8d62a0c0d19
Layton Gilbert <humanhaterq@gmail.com>
2022-07-29 22:26:46 -0700

    Health reduced to 3

--- Cards/crumblingArmour.cs
+++ Cards/crumblingArmour.cs
@@ -18,7 +18,7 @@ namespace WhistleWindLobotomyMod
             CardHelper.CreateCard(
                 "wstl_crumblingArmour", "Crumbling Armour",
                 "A suit of armour that rewards the brave.",
-                0, 4, 0, 6,
+                0, 3, 0, 6,
                 Resources.crumblingArmour, Resources.crumblingArmour_emission,
                 abilities: abilities, specialAbilities: new(),
                 metaCategories: new(), tribes: new(), traits: new(),
