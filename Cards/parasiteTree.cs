4ac5802682bdf1f4d200d738f07fd245e289a0a2
Layton Gilbert <humanhaterq@gmail.com>
2022-07-29 23:24:57 -0700

    Reduced cost to 1 blood

--- Cards/parasiteTree.cs
+++ Cards/parasiteTree.cs
@@ -17,7 +17,7 @@ namespace WhistleWindLobotomyMod
             CardHelper.CreateCard(
                 "wstl_parasiteTree", "Parasite Tree",
                 "A beautiful tree. It wants only to help you and your beasts.",
-                0, 3, 2, 0,
+                0, 3, 1, 0,
                 Resources.parasiteTree, Resources.parasiteTree_emission,
                 abilities: abilities, specialAbilities: new(),
                 metaCategories: new(), tribes: new(), traits: new(),
