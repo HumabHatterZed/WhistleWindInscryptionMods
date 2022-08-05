0cd8ba20751b990d3346bea7207515c2c37ffe21
Layton Gilbert <humanhaterq@gmail.com>
2022-08-05 00:12:51 -0700

    Added rulebook entries for Plague Doctor and Blue Star

---
+++ Abilities/Rulebook/rulebookBlueStar.cs
@@ -0,0 +1,27 @@
+﻿using InscryptionAPI;
+using DiskCardGame;
+using System.Collections;
+using UnityEngine;
+using Resources = WhistleWindLobotomyMod.Properties.Resources;
+
+namespace WhistleWindLobotomyMod
+{
+    public partial class WstlPlugin
+    {
+        private void Rulebook_BlueStar()
+        {
+            const string rulebookName = "Blue Star";
+            const string rulebookDescription = "Omni Strike functions like Moon Strike, striking all opposing slots directly when no cards can block a direct attack.";
+            const string dialogue = "femboy";
+            EntryBlueStar.ability = AbilityHelper.CreateAbility<EntryBlueStar>(
+                Resources.sigilAbnormality, Resources.sigilAbnormality_pixel,
+                rulebookName, rulebookDescription, dialogue, powerLevel: 0,
+                overrideModular: true).Id;
+        }
+    }
+    public class EntryBlueStar : AbilityBehaviour
+    {
+        public static Ability ability;
+        public override Ability Ability => ability;
+    }
+}
---
+++ Abilities/Rulebook/rulebookPlagueDoctor.cs
@@ -0,0 +1,27 @@
+﻿using InscryptionAPI;
+using DiskCardGame;
+using System.Collections;
+using UnityEngine;
+using Resources = WhistleWindLobotomyMod.Properties.Resources;
+
+namespace WhistleWindLobotomyMod
+{
+    public partial class WstlPlugin
+    {
+        private void Rulebook_PlagueDoctor()
+        {
+            const string rulebookName = "Plague Doctor";
+            const string rulebookDescription = "Changes appearance on healing cards.";
+            const string dialogue = "femboy";
+            EntryPlagueDoctor.ability = AbilityHelper.CreateAbility<EntryPlagueDoctor>(
+                Resources.sigilAbnormality, Resources.sigilAbnormality_pixel,
+                rulebookName, rulebookDescription, dialogue, powerLevel: 0,
+                overrideModular: true).Id;
+        }
+    }
+    public class EntryPlagueDoctor : AbilityBehaviour
+    {
+        public static Ability ability;
+        public override Ability Ability => ability;
+    }
+}
