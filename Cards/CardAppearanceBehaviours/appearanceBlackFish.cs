31d5c215091457f43c3f3ae0adbe3d5390a6d5a3
Layton Gilbert <humanhaterq@gmail.com>
2022-08-14 13:19:36 -0700

    Added CardAppearanceBehaviours Deprecated CardPatch

---
+++ Cards/CardAppearanceBehaviours/appearanceBlackFish.cs
@@ -0,0 +1,31 @@
+﻿using InscryptionAPI;
+using DiskCardGame;
+using System.Collections;
+using UnityEngine;
+using Resources = WhistleWindLobotomyMod.Properties.Resources;
+
+namespace WhistleWindLobotomyMod
+{
+    public partial class WstlPlugin
+    {
+        private void Appearance_BlackFish()
+        {
+            BlackFish.appearance = CardHelper.CreateAppearance<BlackFish>("BlackFish").Id;
+        }
+    }
+    public class BlackFish : CardAppearanceBehaviour
+    {
+        public static CardAppearanceBehaviour.Appearance appearance;
+        public override void ApplyAppearance()
+        {
+            if (base.Card is PlayableCard)
+            {
+                base.Card.RenderInfo.portraitOverride = base.Card.Info.alternatePortrait;
+            }
+            else
+            {
+                base.Card.RenderInfo.portraitOverride = null;
+            }
+        }
+    }
+}
---
+++ Cards/CardAppearanceBehaviours/appearanceForced.cs
@@ -0,0 +1,24 @@
+﻿using InscryptionAPI;
+using DiskCardGame;
+using System.Collections;
+using UnityEngine;
+using Resources = WhistleWindLobotomyMod.Properties.Resources;
+
+namespace WhistleWindLobotomyMod
+{
+    public partial class WstlPlugin
+    {
+        private void Appearance_ForcedEmission()
+        {
+            Forced.appearance = CardHelper.CreateAppearance<Forced>("Forced").Id;
+        }
+    }
+    public class Forced : CardAppearanceBehaviour
+    {
+        public static CardAppearanceBehaviour.Appearance appearance;
+        public override void ApplyAppearance()
+        {
+            base.Card.RenderInfo.forceEmissivePortrait = true;
+        }
+    }
+}
