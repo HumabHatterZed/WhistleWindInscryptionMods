ea5b8dfed2c2482dac2375ce8d93e68faeef8da5
Layton Gilbert <humanhaterq@gmail.com>
2022-08-18 13:22:33 -0700

    No longer has fungus decal

--- Cards/theLittlePrinceMinion.cs
+++ Cards/theLittlePrinceMinion.cs
@@ -10,17 +10,13 @@ namespace WhistleWindLobotomyMod
     {
         private void TheLittlePrinceMinion_O0466()
         {
-            List<UnityEngine.Texture> decals = new()
-            {
-                ResourceBank.Get<UnityEngine.Texture>("Art/Cards/Decals/decal_fungus")
-            };
             CardHelper.CreateCard(
                 "wstl_theLittlePrinceMinion", "Spore Mold Creature",
                 "A creature consumed by cruel, kind fungus.",
                 0, 0, 0, 0,
                 Resources.theLittlePrinceMinion, Resources.theLittlePrinceMinion_emission,
                 abilities: new(), specialAbilities: new(),
-                metaCategories: new(), tribes: new(), traits: new(), decals: decals);
+                metaCategories: new(), tribes: new(), traits: new(), decals: new());
         }
     }
 }
