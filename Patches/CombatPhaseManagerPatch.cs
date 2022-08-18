943ddfa5733ec8a6b0c1e315e0700f7cc2ae515b
Layton Gilbert <humanhaterq@gmail.com>
2022-08-17 21:10:16 -0700

    Added opponent sniper AI BiStrike and TriStrike now stack Execution no longer gives bones

--- Patches/CombatPhaseManagerPatch.cs
+++ Patches/CombatPhaseManagerPatch.cs
@@ -3,8 +3,10 @@ using DiskCardGame;
 using HarmonyLib;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using UnityEngine;
+using static WhistleWindLobotomyMod.WstlPlugin;
 
 namespace WhistleWindLobotomyMod
 {
@@ -17,50 +19,181 @@ namespace WhistleWindLobotomyMod
 		private GameObject sniperIconPrefab;
 
 		// nothing I love more than just ripping code verbatim
-		// basically just adds the Marksman ability to the attack sequence code, plus all the special code
+		// Adds Marksman ability support, as well as support for opponent sniper/marksman
 		[HarmonyPostfix, HarmonyPatch(nameof(CombatPhaseManager.SlotAttackSequence))]
 		public static IEnumerator SlotAttackSequence(IEnumerator enumerator, CombatPhaseManager __instance, CardSlot slot)
 		{
-			bool hasMarksman = slot.Card != null && slot.Card.HasAbility(Marksman.ability);
-			bool isJudge = slot.Card != null && slot.Card.Info.name.Equals("wstl_judgementBird");
-			if (slot.Card.OpponentCard || !hasMarksman)
+			bool hasSniper = slot.Card.HasAbility(Ability.Sniper);
+			bool hasMarksman = slot.Card.HasAbility(Marksman.ability);
+			bool isJudge = slot.Card.Info.name.Equals("wstl_judgementBird");
+
+			// If does not have Marksman or Sniper, return normal sequence
+			if (!hasMarksman && !hasSniper)
 			{
-				// Returns normal behaviour for opponent cards
 				yield return enumerator;
 				yield break;
 			}
 
-			List<CardSlot> opposingSlots = new();
+			// Change to combat view
 			Singleton<ViewManager>.Instance.SwitchToView(Singleton<BoardManager>.Instance.CombatView);
 			Singleton<ViewManager>.Instance.Controller.LockState = ViewLockState.Locked;
+
+			// Create list of targets to attack
+			List<CardSlot> targetedSlots = new();
+
+			// Default to 1 attack
 			int numAttacks = 1;
 			if (slot.Card.HasTriStrike())
 			{
-				numAttacks = 3;
+				// Add 2 for TriStrike (total 3)
+				numAttacks += 2;
 			}
-			else if (slot.Card.HasAbility(Ability.SplitStrike))
+			if (slot.Card.HasAbility(Ability.SplitStrike))
 			{
-				numAttacks = 2;
-			}
-			if (slot.Card.HasAbility(Ability.Sniper))
-			{
-				// If a card has both Sniper and Marksman, then it'll add an extra shot. How nice am I.
+				// Add 1 for BiStrike (total 2)
 				numAttacks += 1;
 			}
-		    Singleton<ViewManager>.Instance.Controller.SwitchToControlMode(Singleton<BoardManager>.Instance.ChoosingSlotViewMode);
+			if (hasSniper && hasMarksman)
+			{
+				// Add 1 if both Sniper and Marksman are present
+				numAttacks += 1;
+			}
+
+			// if card is an opponent
+			if (slot.Card.OpponentCard)
+			{
+				List<CardSlot> validSlots = Singleton<BoardManager>.Instance.PlayerSlotsCopy;
+				List<CardSlot> fullPlayerSlots = validSlots.Where((CardSlot s) => s.Card != null).ToList();
+				List<CardSlot> emptyPlayerSlots = validSlots.Where((CardSlot s) => !(s.Card != null)).ToList();
+				
+				int randomSeed = SaveManager.SaveFile.GetCurrentRandomSeed() + Singleton<TurnManager>.Instance.TurnNumber;
+
+				// Attack a random null slot if this card can win the battle
+				if (emptyPlayerSlots.Count() > 0 && slot.Card.Attack >= (10 - Singleton<LifeManager>.Instance.DamageUntilPlayerWin))
+				{
+					CardSlot cardSlot = emptyPlayerSlots[SeededRandom.Range(0, emptyPlayerSlots.Count(), randomSeed)];
+					if (cardSlot != null && targetedSlots.Contains(cardSlot))
+					{
+						CombatPhaseManagerPatch.Instance.VisualizeAimSniperAbility(slot, cardSlot);
+					}
+					targetedSlots.Add(cardSlot);
+					CombatPhaseManagerPatch.Instance.VisualizeConfirmSniperAbility(cardSlot, isJudge);
+					numAttacks--;
+				}
+
+				// Attack all cards that can lose the battle for this card
+				if (numAttacks > 0 && fullPlayerSlots.Count() > 0)
+                {
+					foreach (CardSlot item in fullPlayerSlots.Where(ss => !ss.Card.AttackIsBlocked(ss.opposingSlot) && ss.Card.Attack >= Singleton<LifeManager>.Instance.DamageUntilPlayerWin))
+                    {
+						if (numAttacks > 0)
+                        {
+							CombatPhaseManagerPatch.Instance.VisualizeStartSniperAbility(slot);
+							if (item != null && targetedSlots.Contains(item))
+							{
+								CombatPhaseManagerPatch.Instance.VisualizeAimSniperAbility(slot, item);
+							}
+							targetedSlots.Add(item);
+							CombatPhaseManagerPatch.Instance.VisualizeConfirmSniperAbility(item, isJudge);
+							numAttacks--;
+							if (numAttacks > 0 && item.Card.Health > slot.Card.Attack)
+                            {
+								CombatPhaseManagerPatch.Instance.VisualizeStartSniperAbility(slot);
+								if (item != null && targetedSlots.Contains(item))
+								{
+									CombatPhaseManagerPatch.Instance.VisualizeAimSniperAbility(slot, item);
+								}
+								targetedSlots.Add(item);
+								CombatPhaseManagerPatch.Instance.VisualizeConfirmSniperAbility(item, isJudge);
+								numAttacks--;
+							}
+						}
+                    }
+                }
+
+				// Attack the opposing card if it can kill this card
+				if (numAttacks > 0 && slot.opposingSlot.Card != null && slot.opposingSlot.Card.Attack >= slot.Card.Health)
+				{
+					// check if opposing card has Sharp, Punisher, Serpent's Nest or Reflector
+					bool opposingCanRetaliate = slot.opposingSlot.Card.HasAbility(Ability.Sharp) || slot.opposingSlot.Card.HasAbility(Punisher.ability) ||
+						slot.opposingSlot.Card.HasAbility(SerpentsNest.ability) || slot.opposingSlot.Card.HasAbility(Reflector.ability);
+
+					// if the opposing card can't retaliate or if it can but this card can kill it, add it to targets
+					if (!opposingCanRetaliate || (opposingCanRetaliate && slot.Card.Attack >= slot.opposingSlot.Card.Health))
+					{
+						CombatPhaseManagerPatch.Instance.VisualizeStartSniperAbility(slot);
+						if (slot.opposingSlot != null && targetedSlots.Contains(slot.opposingSlot))
+						{
+							CombatPhaseManagerPatch.Instance.VisualizeAimSniperAbility(slot, slot.opposingSlot);
+						}
+						targetedSlots.Add(slot.opposingSlot);
+						CombatPhaseManagerPatch.Instance.VisualizeConfirmSniperAbility(slot.opposingSlot, isJudge);
+						numAttacks--;
+					}
+				}
+				// Attack a random slot with a bias towards cards
+				for (int i = 0; i < numAttacks; i++)
+                {
+					CombatPhaseManagerPatch.Instance.VisualizeStartSniperAbility(slot);
+					// default to an empty slot
+					CardSlot cardSlot = emptyPlayerSlots.Count() > 0 ? emptyPlayerSlots[SeededRandom.Range(0, emptyPlayerSlots.Count(), randomSeed)] : validSlots[0];
+					randomSeed++;
+
+					// if there are valid cards, change target to one of them at a 75% chance
+					if (fullPlayerSlots.Count() > 0 && SeededRandom.Range(0, 4, randomSeed) > 0)
+                    {
+						cardSlot = fullPlayerSlots[SeededRandom.Range(0, fullPlayerSlots.Count(), randomSeed)];
+					}
+
+					if (cardSlot != null && targetedSlots.Contains(cardSlot))
+					{
+						CombatPhaseManagerPatch.Instance.VisualizeAimSniperAbility(slot, cardSlot);
+					}
+					targetedSlots.Add(cardSlot);
+					CombatPhaseManagerPatch.Instance.VisualizeConfirmSniperAbility(cardSlot, isJudge);
+				}
+
+				// for each target slot, do attack thing
+				foreach (CardSlot item in targetedSlots)
+				{
+					Singleton<ViewManager>.Instance.SwitchToView(Singleton<BoardManager>.Instance.CombatView);
+					if (isJudge && item.Card != null && !item.Card.AttackIsBlocked(slot))
+					{
+						if (slot.Card.FaceDown)
+						{
+							slot.Card.SetFaceDown(false);
+							slot.Card.UpdateFaceUpOnBoardEffects();
+							yield return new WaitForSeconds(0.2f);
+						}
+						slot.Card.Anim.StrongNegationEffect();
+						yield return new WaitForSeconds(0.4f);
+						yield return CombatPhaseManagerPatch.Instance.Execution(item.Card);
+					}
+					else
+					{
+						yield return Singleton<CombatPhaseManager>.Instance.SlotAttackSlot(slot, item, (targetedSlots.Count > 1) ? 0.1f : 0f);
+					}
+				}
+				// End sequence
+				CombatPhaseManagerPatch.Instance.VisualizeClearSniperAbility();
+				yield break;
+			}
+
+			// Non-opponent sequence
+			Singleton<ViewManager>.Instance.Controller.SwitchToControlMode(Singleton<BoardManager>.Instance.ChoosingSlotViewMode);
 			Singleton<ViewManager>.Instance.Controller.LockState = ViewLockState.Unlocked;
 			for (int i = 0; i < numAttacks; i++)
 			{
 				CombatPhaseManagerPatch.Instance.VisualizeStartSniperAbility(slot);
 				CardSlot cardSlot = Singleton<InteractionCursor>.Instance.CurrentInteractable as CardSlot;
 
-				if (cardSlot != null && opposingSlots.Contains(cardSlot))
+				if (cardSlot != null && targetedSlots.Contains(cardSlot))
 				{
 					CombatPhaseManagerPatch.Instance.VisualizeAimSniperAbility(slot, cardSlot);
 				}
 				yield return Singleton<BoardManager>.Instance.ChooseTarget(Singleton<BoardManager>.Instance.OpponentSlotsCopy, Singleton<BoardManager>.Instance.OpponentSlotsCopy, delegate (CardSlot s)
 				{
-					opposingSlots.Add(s);
+					targetedSlots.Add(s);
 					CombatPhaseManagerPatch.Instance.VisualizeConfirmSniperAbility(s, isJudge);
 				}, null, delegate (CardSlot s)
 				{
@@ -68,10 +201,10 @@ namespace WhistleWindLobotomyMod
 
 				}, () => false, isJudge ? CursorType.Sacrifice : CursorType.Target);
 			}
-			foreach (CardSlot item in opposingSlots)
+			foreach (CardSlot item in targetedSlots)
 			{
 				Singleton<ViewManager>.Instance.SwitchToView(Singleton<BoardManager>.Instance.CombatView);
-				if (isJudge && item.Card != null && !item.Card.HasAbility(Ability.PreventAttack))
+				if (isJudge && item.Card != null && !item.Card.AttackIsBlocked(slot))
                 {
 					if (slot.Card.FaceDown)
 					{
@@ -80,12 +213,12 @@ namespace WhistleWindLobotomyMod
 						yield return new WaitForSeconds(0.2f);
 					}
 					slot.Card.Anim.StrongNegationEffect();
-					yield return new WaitForSeconds(0.6f);
+					yield return new WaitForSeconds(0.4f);
 					yield return CombatPhaseManagerPatch.Instance.Execution(item.Card);
 				}
 				else
                 {
-					yield return Singleton<CombatPhaseManager>.Instance.SlotAttackSlot(slot, item, (opposingSlots.Count > 1) ? 0.1f : 0f);
+					yield return Singleton<CombatPhaseManager>.Instance.SlotAttackSlot(slot, item, (targetedSlots.Count > 1) ? 0.1f : 0f);
 				}
 			}
 			Singleton<ViewManager>.Instance.Controller.SwitchToControlMode(Singleton<BoardManager>.Instance.DefaultViewMode);
@@ -130,7 +263,6 @@ namespace WhistleWindLobotomyMod
 			target.Anim.PlaySacrificeSound();
 			target.Anim.DeactivateSacrificeHoverMarker();
 			yield return target.Die(wasSacrifice: false);
-			yield return Singleton<ResourcesManager>.Instance.AddBones(1, target.Slot);
 		}
 	}
 }
