732f3504ffa030156d7dfef85e84c835048ef989
Layton Gilbert <humanhaterq@gmail.com>
2022-08-21 13:01:02 -0700

    Removed duplicate patch

--- Patches/CustomMechanicsPatches.cs
+++ Patches/CustomMechanicsPatches.cs
@@ -32,20 +32,6 @@ namespace WhistleWindLobotomyMod
             yield return enumerator;
         }
     }
-    [HarmonyPatch(typeof(PlayableCard))]
-    public class PlayableCardPatch
-    {
-        // Increases damage taken by amount of Prudence a card has
-        [HarmonyPostfix, HarmonyPatch(nameof(PlayableCard.TakeDamage))]
-        public static void TakePrudenceDamage(PlayableCard __instance, ref int damage)
-        {
-            int prudence = !(__instance.Info.GetExtendedPropertyAsInt("wstl:Prudence") != null) ? 0 : (int)__instance.Info.GetExtendedPropertyAsInt("wstl:Prudence");
-            if (prudence > 0)
-            {
-                damage += prudence;
-            }
-        }
-    }
     [HarmonyPatch(typeof(BoardManager))]
     public static class BoardManagerPatch
     {
