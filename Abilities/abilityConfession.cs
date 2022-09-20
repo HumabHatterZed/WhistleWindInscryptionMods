53a9867495463d06cf2dfb21eeafe6790c1c4e07
Layton Gilbert <humanhaterq@gmail.com>
2022-09-20 11:55:30 -0700

    Updated description to reflect current behaviour

--- Abilities/abilityConfession.cs
+++ Abilities/abilityConfession.cs
@@ -13,7 +13,7 @@ namespace WhistleWindLobotomyMod
         private void Ability_Confession()
         {
             const string rulebookName = "Confession and Pentinence";
-            string rulebookDescription = ConfigManager.Instance.RevealWhiteNight ? "Kills the Heretic and creates a special card. If used on the special card, kill WhiteNight, his Apostles, and deal 33 direct damage." : "Activate: Keep faith with unwavering resolve.";
+            string rulebookDescription = ConfigManager.Instance.RevealWhiteNight ? "Kills the Heretic and summons One Sin. Kills WhiteNight and his Apostles then deals 33 direct damage." : "Activate: Keep faith with unwavering resolve.";
             const string dialogue = "[c:bG]Keep faith with unwavering resolve.[c:]";
 
             Confession.ability = AbilityHelper.CreateActivatedAbility<Confession>(
