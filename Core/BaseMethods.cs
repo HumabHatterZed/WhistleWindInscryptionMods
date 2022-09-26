f9c272c6e16f5c608b53661a4a1942685a97bbee
HumabHatterZed <69230920+HumabHatterZed@users.noreply.github.com>
2022-09-26 10:22:33 -0700

    Update BaseMethods.cs

--- Core/BaseMethods.cs
+++ Core/BaseMethods.cs
@@ -16,7 +16,7 @@ namespace WhistleWindLobotomyMod
                 List<CardSlot> openSlots = Singleton<BoardManager>.Instance.OpponentSlotsCopy.Where(s => !Singleton<TurnManager>.Instance.Opponent.QueuedSlots.Contains(s)).ToList();
                 if (openSlots.Count() == 0)
                 {
-                    WstlPlugin.Log.LogDebug($"Appending {CardToDraw.name} to end of turn plan.");
+                    WstlPlugin.Log.LogDebug($"Appending {cardToQueue.name} to end of turn plan.");
                     List<List<CardInfo>> turnPlan = Singleton<TurnManager>.Instance.Opponent.TurnPlan;
                     List<CardInfo> addInfo = new() { cardToQueue };
                     turnPlan.Add(addInfo);
@@ -24,9 +24,9 @@ namespace WhistleWindLobotomyMod
                 }
                 else
                 {
-                    WstlPlugin.Log.LogDebug($"Adding {CardToDraw.name} to queue.");
+                    WstlPlugin.Log.LogDebug($"Adding {cardToQueue.name} to queue.");
                     CardSlot index = openSlots[SeededRandom.Range(0, openSlots.Count, randomSeed++)];
-                    yield return Singleton<TurnManager>.Instance.Opponent.QueueCard(CardToDraw, index);
+                    yield return Singleton<TurnManager>.Instance.Opponent.QueueCard(cardToQueue, index);
                 }
                 yield return new WaitForSeconds(0.45f);
         }
