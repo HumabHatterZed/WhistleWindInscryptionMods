5ef3559662352c830a63df91a02c34bd319735cd
WhistleWind <humanhaterq@gmail.com>
2023-01-31 22:50:23 -0800

    Sigil Power now only affects Power, excludes Random

--- WhistleWind.AbnormalSigils/Abilities/Stat Icons/variableSigilPower.cs
+++ WhistleWind.AbnormalSigils/Abilities/Stat Icons/variableSigilPower.cs
@@ -17,22 +17,11 @@ namespace WhistleWind.AbnormalSigils
         public override int[] GetStatValues()
         {
             List<AbilityInfo> infos = AbilityManager.AllAbilityInfos
-                .FindAll(x => base.PlayableCard.AllAbilities().Contains(x.ability));
+                .FindAll(x => x.ability != Ability.RandomAbility && base.PlayableCard.AllAbilities().Contains(x.ability));
+
             infos.Sort((AbilityInfo a, AbilityInfo b) => b.powerLevel - a.powerLevel);
 
-            int sigilPower = infos[0].powerLevel;
-
-            // if sigil power is negative
-            if (sigilPower < 0)
-            {
-                // if sigil power would kill the card, initiate death
-                if (base.PlayableCard.Health - sigilPower <= 0)
-                    base.Card.StartCoroutine(base.PlayableCard.Die(false));
-                
-                return new int[2] { 0, sigilPower };
-            }
-
-            return new int[2] { sigilPower, sigilPower };
+            return new int[2] { infos[0].powerLevel, 0 };
         }
     }
 
@@ -41,11 +30,11 @@ namespace WhistleWind.AbnormalSigils
         private void StatIcon_SigilPower()
         {
             const string rulebookName = "Sigil Power";
-            const string rulebookDescription = "The value represented by this sigil will be equal to the powerlevel of this card's strongest base sigil.";
+            const string rulebookDescription = "The value represented by this sigil will be equal to the powerlevel of this card's strongest sigil.";
             SigilPower.icon = AbilityHelper.CreateStatIcon<SigilPower>(
                 pluginGuid, rulebookName, rulebookDescription,
-                (UnityEngine.Texture2D)AbilitiesUtil.LoadAbilityIcon(Ability.RandomAbility.ToString()),
-                TextureLoader.LoadTextureFromBytes(Artwork.sigilSigilPower_pixel), true, true).Id;
+                TextureLoader.LoadTextureFromBytes(Artwork.sigilSigilPower),
+                TextureLoader.LoadTextureFromBytes(Artwork.sigilSigilPower_pixel), true, false).Id;
         }
     }
 }
