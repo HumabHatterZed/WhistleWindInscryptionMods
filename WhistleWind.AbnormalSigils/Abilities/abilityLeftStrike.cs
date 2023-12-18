ecf7d5efeba8539be04073976095e64f354e75d0
WhistleWind <humanhaterq@gmail.com>
2023-12-17 18:39:45 -0800

    Added Persecutor, Left- and Right-Veering Strike sigils

---
+++ WhistleWind.AbnormalSigils/Abilities/abilityLeftStrike.cs
@@ -0,0 +1,47 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Card;
+using InscryptionAPI.Helpers.Extensions;
+using InscryptionAPI.Triggers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WhistleWind.AbnormalSigils.Core.Helpers;
+
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public partial class AbnormalPlugin
+    {
+        private void Ability_LeftStrike()
+        {
+            const string rulebookName = "Left-Veering Strike";
+            const string rulebookDescription = "[creature] will strike the opposing space to the left of the space across from it.";
+            LeftStrike.ability = AbnormalAbilityHelper.CreateAbility<LeftStrike>(
+                "sigilLeftStrike",
+                rulebookName, rulebookDescription, powerLevel: 0,
+                modular: false, opponent: false, canStack: false).Id;
+        }
+    }
+    public class LeftStrike : AbilityBehaviour, ISetupAttackSequence
+    {
+        public static Ability ability;
+        public override Ability Ability => ability;
+
+        public List<CardSlot> CollectModifyAttackSlots(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, ref int attackCount, ref bool didRemoveDefaultSlot)
+        {
+            currentSlots.Remove(base.Card.OpposingSlot());
+            if (base.Card.OpposingSlot().GetAdjacent(true) != null)
+                currentSlots.Add(base.Card.OpposingSlot().GetAdjacent(true));
+
+            return currentSlots;
+        }
+
+        public int GetTriggerPriority(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, int attackCount, bool didRemoveDefaultSlot) => 0;
+
+        public bool RespondsToModifyAttackSlots(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, int attackCount, bool didRemoveDefaultSlot)
+        {
+            return card == base.Card && modType == OpposingSlotTriggerPriority.Normal;
+        }
+    }
+}
---
+++ WhistleWind.AbnormalSigils/Abilities/abilityPersecutor.cs
@@ -0,0 +1,84 @@
+﻿using DiskCardGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WhistleWind.AbnormalSigils.Core.Helpers;
+
+
+namespace WhistleWind.AbnormalSigils
+{
+    public partial class AbnormalPlugin
+    {
+        private void Ability_Persecutor()
+        {
+            const string rulebookName = "Persecutor";
+            const string rulebookDescription = "When a card bearing this sigil is played, create a Nail in the adjacent left space and a Hammer in the adjacent right space if they are empty.";
+            const string dialogue = "Are you guilty of having a closed heart?";
+            const string triggerText = "[creature] reveals its hidden tools!";
+            Persecutor.ability = AbnormalAbilityHelper.CreateAbility<Persecutor>(
+                "sigilPersecutor",
+                rulebookName, rulebookDescription, dialogue, triggerText, powerLevel: 4,
+                modular: false, opponent: false, canStack: false).Id;
+        }
+    }
+    public class Persecutor : AbilityBehaviour
+    {
+        public static Ability ability;
+        public override Ability Ability => ability;
+
+        private readonly string CannotSpawnDialogue = "These tools remain hidden for now.";
+
+        public override bool RespondsToResolveOnBoard() => true;
+
+        public override IEnumerator OnResolveOnBoard()
+        {
+            Singleton<ViewManager>.Instance.SwitchToView(View.Board);
+            CardSlot toLeft = Singleton<BoardManager>.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: true);
+            CardSlot toRight = Singleton<BoardManager>.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: false);
+            bool toLeftValid = toLeft != null && toLeft.Card == null;
+            bool toRightValid = toRight != null && toRight.Card == null;
+            yield return base.PreSuccessfulTriggerSequence();
+            if (toLeftValid)
+            {
+                yield return new WaitForSeconds(0.1f);
+                yield return this.SpawnCardOnSlot(toLeft, true);
+            }
+            if (toRightValid)
+            {
+                yield return new WaitForSeconds(0.1f);
+                yield return this.SpawnCardOnSlot(toRight, false);
+            }
+
+            if (toLeftValid || toRightValid)
+                yield return base.LearnAbility();
+
+            else if (!base.HasLearned)
+                yield return Singleton<TextDisplayer>.Instance.ShowUntilInput(this.CannotSpawnDialogue, -0.65f, 0.4f);
+        }
+
+        private IEnumerator SpawnCardOnSlot(CardSlot slot, bool left)
+        {
+            CardInfo cardByName = CardLoader.GetCardByName(left ? "wstl_nail" : "wstl_hammer");
+            this.ModifySpawnedCard(cardByName);
+            yield return Singleton<BoardManager>.Instance.CreateCardInSlot(cardByName, slot, 0.15f);
+        }
+
+        private void ModifySpawnedCard(CardInfo card)
+        {
+            List<Ability> abilities = base.Card.Info.Abilities;
+            foreach (CardModificationInfo temporaryMod in base.Card.TemporaryMods)
+                abilities.AddRange(temporaryMod.abilities);
+
+            abilities.RemoveAll((Ability x) => x == this.Ability);
+            if (abilities.Count > 4)
+                abilities.RemoveRange(3, abilities.Count - 4);
+
+            CardModificationInfo cardModificationInfo = new()
+            {
+                fromCardMerge = true,
+                abilities = abilities
+            };
+            card.Mods.Add(cardModificationInfo);
+        }
+    }
+}
