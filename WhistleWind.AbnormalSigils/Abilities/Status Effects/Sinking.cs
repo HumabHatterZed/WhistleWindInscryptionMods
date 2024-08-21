cee8c7132235c7ea9444df1eeb0185fe9c03f21a
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2024-08-21 10:14:44 -0700

    Added Sinking and placeholder icons

---
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/Sinking.cs
@@ -0,0 +1,48 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Helpers.Extensions;
+using InscryptionAPI.Triggers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WhistleWind.AbnormalSigils.StatusEffects;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public class Sinking : StatusEffectBehaviour
+    {
+        public static Ability iconId;
+        public static SpecialTriggeredAbility specialAbility;
+        public override Ability IconAbility => iconId;
+        public override SpecialTriggeredAbility StatusEffect => specialAbility;
+
+        public override bool RespondsToTakeDamage(PlayableCard source) => true;
+
+        public override IEnumerator OnTakeDamage(PlayableCard source)
+        {
+            base.Card.HealDamage(-EffectPotency);
+            base.DestroyStatusEffect();
+            if (base.Card.Health >= 0)
+            {
+                yield return base.Card.Die(false, source);
+            }
+            yield break;
+        }
+    }
+    public partial class AbnormalPlugin
+    {
+        private void StatusEffect_Sinking()
+        {
+            const string rName = "Sinking";
+            const string rDesc = "This card loses Power equal to its Sinking. When this card is struck, take additional damage equal to its Sinking then remove this effect.";
+            StatusEffectManager.FullStatusEffect data = StatusEffectManager.New<Sinking>(
+                pluginGuid, rName, rDesc, 2, GameColors.Instance.nearWhite,
+                TextureLoader.LoadTextureFromFile("sigilSinking.png", Assembly),
+                TextureLoader.LoadTextureFromFile("sigilSinking_pixel.png", Assembly))
+                .AddMetaCategories(StatusMetaCategory.Part1StatusEffect);
+
+            Sinking.specialAbility = data.Id;
+            Sinking.iconId = data.IconInfo.ability;
+        }
+    }
+}
