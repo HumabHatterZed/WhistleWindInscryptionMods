813741aa137b7f10eca6d04ed8128ad368308799
WhistleWind <humanhaterq@gmail.com>
2024-09-05 14:12:38 -0700

    Renamed Mind Flayer to Mind Strike

--- WhistleWind.AbnormalSigils/Abilities/MindStrike.cs
+++ WhistleWind.AbnormalSigils/Abilities/MindStrike.cs
@@ -1,28 +1,32 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Card;
+using InscryptionAPI.Rulebook;
+using InscryptionAPI.Triggers;
 using System.Collections;
 using UnityEngine;
 using WhistleWind.AbnormalSigils.Core.Helpers;
-
+using WhistleWind.AbnormalSigils.Patches;
+using WhistleWind.AbnormalSigils.StatusEffects;
 using WhistleWind.Core.Helpers;
 
 namespace WhistleWind.AbnormalSigils
 {
     public partial class AbnormalPlugin
     {
-        private void Ability_MindFlayer()
+        private void Ability_MindStrike()
         {
-            const string rulebookName = "Mind Flayer";
+            const string rulebookName = "Mind Strike";
             const string rulebookDescription = "When [creature] strikes another creature, deal no damage and inflict Sinking equal to half this card's Health, rounded up.";
             const string dialogue = "Why destroy the flesh when you can destroy the mind?";
             const string triggerText = "[creature] deals emotional damage!";
-            MindFlayer.ability = AbnormalAbilityHelper.CreateAbility<MindFlayer>(
+            MindStrike.ability = AbnormalAbilityHelper.CreateAbility<MindStrike>(
                 "sigilMindFlayer",
                 rulebookName, rulebookDescription, dialogue, triggerText, powerLevel: 2,
-                modular: false, opponent: true, canStack: false).Id;
+                modular: false, opponent: true, canStack: false).SetAbilityRedirect("Sinking", Sinking.iconId, GameColors.Instance.seafoam).Info
+                .AddMetaCategories(AbilityMetaCategory.GrimoraRulebook, AbilityMetaCategory.MagnificusRulebook, AbilityMetaCategory.Part3Rulebook).ability;
         }
     }
-    public class MindFlayer : AbilityBehaviour, IModifyDamageTaken
+    public class MindStrike : AbilityBehaviour, IModifyDamageTaken
     {
         public static Ability ability;
         public override Ability Ability => ability;
@@ -32,7 +36,6 @@ namespace WhistleWind.AbnormalSigils
             yield return target.AddStatusEffect<Sinking>((base.Card.Health + 1) / 2);
             yield return base.LearnAbility(0.3f);
         }
-
         public bool RespondsToModifyDamageTaken(PlayableCard target, int damage, PlayableCard attacker, int originalDamage) => attacker == base.Card;
         public int OnModifyDamageTaken(PlayableCard target, int damage, PlayableCard attacker, int originalDamage) => 0;
         public int TriggerPriority(PlayableCard target, int damage, PlayableCard attacker) => -1000;
