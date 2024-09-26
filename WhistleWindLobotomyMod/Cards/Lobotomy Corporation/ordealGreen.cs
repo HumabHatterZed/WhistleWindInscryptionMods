86d9094208b55a6e14dccac42a2cf244ad90f973
WhistleWind <humanhaterq@gmail.com>
2024-09-25 21:56:00 -0700

    Art work begin for green ordeal cards

--- WhistleWindLobotomyMod/Cards/Lobotomy Corporation/ordealGreen.cs
+++ WhistleWindLobotomyMod/Cards/Lobotomy Corporation/ordealGreen.cs
@@ -20,40 +20,48 @@ namespace WhistleWindLobotomyMod
             const string whereWeReach = "whereWeReach";
             const string lastHelix = "lastHelix";
 
-            CardInfo infoO = CardManager.New(pluginPrefix, doubtO, "Doubt δ",
+            CardInfo infoO = CardManager.New(pluginPrefix, doubtO, "Doubt O",
                 attack: 2, health: 4)
                 .SetEnergyCost(4)
                 .SetPortraits(ModAssembly, doubtO)
+                .SetTitle(ModAssembly, "doubtO_title.png")
                 .AddAbilities(Piercing.ability, Ability.MadeOfStone)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
                 .AddTribes(TribeMechanical)
                 .AddTraits(Ordeal)
                 .Build();
 
-            CardInfo infoY = CardManager.New(pluginPrefix, doubtY, "Doubt γ",
+            CardInfo infoY = CardManager.New(pluginPrefix, doubtY, "Doubt Y",
                 attack: 1, health: 3)
                 .SetEnergyCost(3)
                 .SetPortraits(ModAssembly, doubtY)
+                .SetTitle(ModAssembly, "doubtY_title.png")
                 .AddAbilities(Piercing.ability, Ability.MadeOfStone)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
                 .AddTribes(TribeMechanical)
                 .SetEvolve(infoO, 1)
                 .AddTraits(Ordeal)
                 .Build();
 
-            CardInfo infoB = CardManager.New(pluginPrefix, doubtB, "Doubt β",
+            CardInfo infoB = CardManager.New(pluginPrefix, doubtB, "Doubt B",
                 attack: 1, health: 2)
                 .SetEnergyCost(2)
                 .SetPortraits(ModAssembly, doubtB)
+                .SetTitle(ModAssembly, "doubtB_title.png")
                 .AddAbilities(Piercing.ability, Ability.MadeOfStone)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
                 .AddTribes(TribeMechanical)
                 .SetEvolve(infoY, 1)
                 .AddTraits(Ordeal)
                 .Build();
 
-            CardManager.New(pluginPrefix, doubtA, "Doubt α",
+            CardManager.New(pluginPrefix, doubtA, "Doubt A",
                 attack: 1, health: 1)
                 .SetEnergyCost(2)
                 .SetPortraits(ModAssembly, doubtA)
+                .SetTitle(ModAssembly, "doubtA_title.png")
                 .AddAbilities(Piercing.ability, Ability.MadeOfStone)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
                 .AddTribes(TribeMechanical)
                 .SetEvolve(infoB, 1)
                 .AddTraits(Ordeal)
