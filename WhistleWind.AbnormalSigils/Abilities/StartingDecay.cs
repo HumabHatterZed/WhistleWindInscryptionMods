38d34cf6dddd7643801e7dad84ee66f8b4608880
WhistleWind <humanhaterq@gmail.com>
2024-10-02 00:08:23 -0700

    Added Decay and Imminent Decay

---
+++ WhistleWind.AbnormalSigils/Abilities/StartingDecay.cs
@@ -0,0 +1,40 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Card;
+using InscryptionAPI.Helpers.Extensions;
+using InscryptionAPI.RuleBook;
+using System.Collections;
+using UnityEngine;
+using WhistleWind.AbnormalSigils.Core.Helpers;
+using WhistleWind.AbnormalSigils.StatusEffects;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public partial class AbnormalPlugin
+    {
+        private void Ability_StartingDecay()
+        {
+            const string rulebookName = "Imminent Decay";
+            const string rulebookDescription = "When [creature] is played, it gains 1 Decay for each stack of this sigil.";
+            StartingDecay.ability = AbnormalAbilityHelper.CreateAbility<StartingDecay>(
+                "sigilDecay",
+                rulebookName, rulebookDescription, powerLevel: -3,
+                modular: false, opponent: false, canStack: true)
+                .SetAbilityRedirect("Decay", Decay.iconId, GameColors.Instance.darkPurple)
+                .SetPart3Rulebook()
+                .SetGrimoraRulebook()
+                .SetMagnificusRulebook().Id;
+        }
+    }
+    public class StartingDecay : AbilityBehaviour
+    {
+        public static Ability ability;
+        public override Ability Ability => ability;
+
+        public override bool RespondsToResolveOnBoard() => base.Card.LacksTrait(AbnormalPlugin.ImmuneToAilments);
+        public override IEnumerator OnResolveOnBoard()
+        {
+            yield return base.Card.AddStatusEffect<Decay>(1);
+        }
+    }
+}
---
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/Decay.cs
@@ -0,0 +1,42 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Card;
+using System.Collections;
+using WhistleWind.AbnormalSigils.StatusEffects;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public class Decay : ModifyOnUpkeepStatusEffectBehaviour
+    {
+        public static Ability iconId;
+        public static SpecialTriggeredAbility specialAbility;
+        public override Ability IconAbility => iconId;
+        public override SpecialTriggeredAbility StatusEffect => specialAbility;
+        public override int PotencyModification => -1;
+        public override IEnumerator OnModifyOnUpkeep()
+        {
+            yield return base.OnModifyOnUpkeep();
+            if (PotencyModification <= 0)
+            {
+                yield return base.PlayableCard.Die(false, base.PlayableCard);
+            }
+        }
+    }
+    public partial class AbnormalPlugin
+    {
+        private void StatusEffect_Decay()
+        {
+            const string rName = "Decay";
+            const string rDesc = "At the start of the owner's turn, reduce this effect's Potency by 1. A card bearing this effect will perish when its Potency reaches 0.";
+            StatusEffectManager.FullStatusEffect data = StatusEffectManager.New<Decay>(
+                pluginGuid, rName, rDesc, -3, GameColors.Instance.darkPurple,
+                TextureLoader.LoadTextureFromFile("sigilDecay.png", Assembly),
+                TextureLoader.LoadTextureFromFile("sigilDecay_pixel.png", Assembly))
+                .AddMetaCategories(StatusMetaCategory.Part1StatusEffect, StatusMetaCategory.Part3StatusEffect, StatusMetaCategory.GrimoraStatusEffect, StatusMetaCategory.MagnificusStatusEffect, StatusMetaCategory.MagnificusStatusEffect)
+                .SetIrremovable(true);
+
+            Decay.specialAbility = data.Id;
+            Decay.iconId = data.IconInfo.ability;
+        }
+    }
+}
