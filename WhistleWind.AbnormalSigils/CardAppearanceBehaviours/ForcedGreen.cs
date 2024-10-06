f4c114887939b35b9ecdf3982a431cff5633dc26
WhistleWind <humanhaterq@gmail.com>
2024-10-05 18:07:48 -0700

    renamed files, added more appearances

---
+++ WhistleWind.AbnormalSigils/CardAppearanceBehaviours/ForcedGreen.cs
@@ -0,0 +1,22 @@
+﻿using DiskCardGame;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public partial class AbnormalPlugin
+    {
+        private void Appearance_GreenEmission()
+        {
+            ForcedGreenEmission.appearance = CardHelper.CreateAppearance<ForcedGreenEmission>(pluginGuid, "ForcedGreenEmission").Id;
+        }
+    }
+    public class ForcedGreenEmission : CardAppearanceBehaviour
+    {
+        public static Appearance appearance;
+        public override void ApplyAppearance()
+        {
+            base.Card.RenderInfo.forceEmissivePortrait = true;
+            base.Card.StatsLayer.SetEmissionColor(GameColors.Instance.brightLimeGreen);
+        }
+    }
+}
---
+++ WhistleWind.AbnormalSigils/CardAppearanceBehaviours/ForcedPurple.cs
@@ -0,0 +1,22 @@
+﻿using DiskCardGame;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public partial class AbnormalPlugin
+    {
+        private void Appearance_PurpleEmission()
+        {
+            ForcedPurpleEmission.appearance = CardHelper.CreateAppearance<ForcedPurpleEmission>(pluginGuid, "ForcedPurpleEmission").Id;
+        }
+    }
+    public class ForcedPurpleEmission : CardAppearanceBehaviour
+    {
+        public static Appearance appearance;
+        public override void ApplyAppearance()
+        {
+            base.Card.RenderInfo.forceEmissivePortrait = true;
+            base.Card.StatsLayer.SetEmissionColor(GameColors.Instance.purple);
+        }
+    }
+}
