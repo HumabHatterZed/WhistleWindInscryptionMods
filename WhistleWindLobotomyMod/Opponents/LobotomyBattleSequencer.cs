0eaf3be9122984970fb6b0038bde79c8b1b2af6e
WhistleWind <humanhaterq@gmail.com>
2024-10-08 22:26:43 -0700

    moved target stuff to parent class

--- WhistleWindLobotomyMod/Opponents/LobotomyBattleSequencer.cs
+++ WhistleWindLobotomyMod/Opponents/LobotomyBattleSequencer.cs
@@ -1,5 +1,6 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Triggers;
+using Pixelplacement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,9 @@ namespace WhistleWindLobotomyMod.Opponents
     {
         public int currentExcessBones = 0;
         public bool drewInitialHand = false;
+
+        public GameObject targetIconPrefab = ResourceBank.Get<GameObject>("Prefabs/Cards/SpecificCardModels/CannonTargetIcon");
+        public readonly List<GameObject> targetIcons = new();
 
         public virtual bool PreventScaleDamage { get; set; } = true;
         public virtual bool DirectDamageGivesBones { get; set; } = true;
@@ -40,6 +44,33 @@ namespace WhistleWindLobotomyMod.Opponents
             }
 
             return damage;
+        }
+
+        public void CreateTargetIcon(CardSlot targetSlot, Color materialColour = default)
+        {
+            GameObject gameObject = Instantiate(targetIconPrefab, targetSlot.transform);
+            gameObject.transform.localPosition = new Vector3(0f, 0.25f, 0f);
+            gameObject.transform.localRotation = Quaternion.identity;
+
+            if (materialColour != default)
+                gameObject.GetComponentInChildren<MeshRenderer>().material.color = materialColour;
+
+            targetIcons.Add(gameObject);
+        }
+        public void CleanupTargetIcons()
+        {
+            targetIcons.ForEach(delegate (GameObject x)
+            {
+                if (x != null) CleanUpTargetIcon(x);
+            });
+            targetIcons.Clear();
+        }
+        public void CleanUpTargetIcon(GameObject icon)
+        {
+            Tween.LocalScale(icon.transform, Vector3.zero, 0.1f, 0f, Tween.EaseIn, Tween.LoopType.None, null, delegate
+            {
+                Destroy(icon);
+            });
         }
 
         public override IEnumerator PlayerCombatEnd()
