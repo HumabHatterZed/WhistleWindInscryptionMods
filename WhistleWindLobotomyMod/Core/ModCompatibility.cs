0f86be4de24aad9798900cc604f38a1c88d170e3
WhistleWind <humanhaterq@gmail.com>
2025-01-28 21:39:21 -0800

    Pack Manager pack art changed

--- WhistleWindLobotomyMod/Core/ModCompatibility.cs
+++ WhistleWindLobotomyMod/Core/ModCompatibility.cs
@@ -26,29 +26,29 @@ namespace WhistleWindLobotomyMod
             PackManager.AddProtectedMetacategory(DonatorCard);
 
             PackInfo pack = PackManager.GetPackInfo<PackInfo>(LobotomyPlugin.pluginPrefix);
+            pack.ValidFor.Clear();
             pack.ValidFor.Add(PackInfo.PackMetacategory.LeshyPack);
             pack.Title = "World of L Corp HQ";
-            pack.Description = $"A set of {BaseModCards.Count(x => x.HasAnyOfCardMetaCategories(DiskCardGame.CardMetaCategory.ChoiceNode, DiskCardGame.CardMetaCategory.Rare))} abnormalities hailing from L Corp HQ and the Library.";
+            pack.Description = $"A set of 88 abnormalities hailing from L Corp HQ and the Library.";
             pack.SetTexture(TextureLoader.LoadTextureFromFile("wstl_pack.png"));
 
-            PackInfo pack2 = PackManager.GetPackInfo<PackInfo>(LobotomyPlugin.wonderlabPrefix);
+            /*PackInfo pack2 = PackManager.GetPackInfo<PackInfo>(LobotomyPlugin.wonderlabPrefix);
+            pack.ValidFor.Clear();
             pack2.ValidFor.Add(PackInfo.PackMetacategory.LeshyPack);
-            pack2.Title = "World of L Corp Branches";
-            pack2.Description = $"A set of {WonderLabCards.Count(x => x.HasAnyOfCardMetaCategories(DiskCardGame.CardMetaCategory.ChoiceNode, DiskCardGame.CardMetaCategory.Rare))} abnormalities primarily originating from Branch O-5681.";
-            pack2.SetTexture(TextureLoader.LoadTextureFromFile("wstl_pack2.png"));
+            pack2.Title = "World of WonderLab";
+            pack2.Description = $"A set of {WonderLabCards.Count(x => x.HasAnyOfCardMetaCategories(DiskCardGame.CardMetaCategory.ChoiceNode, DiskCardGame.CardMetaCategory.Rare))} abnormalities originating primarily from Branch O-5681.";
+            pack2.SetTexture(TextureLoader.LoadTextureFromFile("wstl_pack_wl.png"));*/
 
             /*PackInfo pack3 = PackManager.GetPackInfo<PackInfo>(LobotomyPlugin.limbusPrefix);
             pack3.ValidFor.Add(PackInfo.PackMetacategory.LeshyPack);
             pack3.Title = "World of Limbus Company";
-            pack3.Description = $"A set of {LimbusCards.Count} abnormalities as recorded by the LCB manager.";
-            pack3.SetTexture(TextureLoader.LoadTextureFromFile("wstl_pack2.png"));*/
+            pack3.Description = $"A set of {LimbusCards.Count} abnormalities encountered by Limbus Company's LCB team.";
+            pack3.SetTexture(TextureLoader.LoadTextureFromFile("wstl_pack_lc.png"));*/
 
-            /*                EncounterPackInfo encounterPack = PackManager.GetPackInfo<EncounterPackInfo>(pluginPrefix);
-                            encounterPack.ValidFor.Add(PackInfo.PackMetacategory.LeshyPack);
-                            encounterPack.Title = pluginName;
-                            encounterPack.Description = "A set of [summary] exclusively featuring abnormalities and related phenomena.";*/
-            //encounterPack.SetTexture(TextureLoader.LoadTextureFromFile("wstl_pack"));
-
+            EncounterPackInfo encounterPack = PackManager.GetPackInfo<EncounterPackInfo>(LobotomyPlugin.pluginPrefix);
+            encounterPack.ValidFor.Add(PackInfo.PackMetacategory.LeshyPack);
+            encounterPack.Title = "Receptions of L Corp HQ";
+            encounterPack.Description = "A set of [summary] encounters exclusively featuring abnormalities and related phenomena.";
         }
     }
 
