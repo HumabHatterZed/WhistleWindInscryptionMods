8620297cf514a48d7dec2a3ded8140cf680aec09
WhistleWind <humanhaterq@gmail.com>
2025-02-08 22:23:40 -0800

    Added TargetIconHelper

---
+++ WhistleWind.AbnormalSigils/Core/TargetIconHelper.cs
@@ -0,0 +1,33 @@
+﻿using DiskCardGame;
+using Pixelplacement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace WhistleWind.AbnormalSigils.Core
+{
+    public class TargetIconHelper
+    {
+        public static GameObject targetIconPrefab = ResourceBank.Get<GameObject>("Prefabs/Cards/SpecificCardModels/CannonTargetIcon");
+
+        public static GameObject CreateTargetIcon(CardSlot targetSlot, Color materialColour = default)
+        {
+            GameObject gameObject = GameObject.Instantiate(targetIconPrefab, targetSlot.transform);
+            gameObject.transform.localPosition = new Vector3(0f, 0.25f, 0f);
+            gameObject.transform.localRotation = Quaternion.identity;
+
+            if (materialColour != default)
+                gameObject.GetComponentInChildren<MeshRenderer>().material.color = materialColour;
+
+            return gameObject;
+        }
+        public static void CleanUpTargetIcon(GameObject icon)
+        {
+            Tween.LocalScale(icon.transform, Vector3.zero, 0.1f, 0f, Tween.EaseIn, Tween.LoopType.None, null, delegate
+            {
+                GameObject.Destroy(icon);
+            });
+        }
+    }
+}
