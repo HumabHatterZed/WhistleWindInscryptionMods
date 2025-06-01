f2943bcee10d51b26a1a734d51f5a4a10a143f7b
WhistleWind <humanhaterq@gmail.com>
2025-05-31 22:29:01 -0700

    Fixed counter text not desyncing with active/inactive state

--- WhistleWindLobotomyMod/Opponents/Ordeals/OrdealCounterManager.cs
+++ WhistleWindLobotomyMod/Opponents/Ordeals/OrdealCounterManager.cs
@@ -74,10 +74,9 @@ namespace WhistleWindLobotomyMod
             {
                 amountLeft = 0;
                 leftRenderer.sprite = null;
-                counterText.text = "000";
+                counterText.text = "";
                 counterText.color = Color.black;
                 this.anim.Play("exit", 0, 0f);
-                //Tween.Position(this.transform, new(0f, -4.5f, 9f), 0.5f, 0f);
                 Tween.Position(LeshyAnimationController.Instance.transform, new Vector3(0f, 4.75f, 9f), 1f, 0.5f);
                 CustomCoroutine.WaitThenExecute(0.5f, delegate
                 {
