b75b1f63c3205b52d142bd4acbcd8432c83c8742
WhistleWind <humanhaterq@gmail.com>
2025-06-03 20:25:22 -0700

    Added Apostle Grizzly challenge (stub)

---
+++ WhistleWindLobotomyMod/Challenges/ApostleGrizzlies.cs
@@ -0,0 +1,26 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Ascension;
+using WhistleWind.Core.Helpers;
+
+
+namespace WhistleWindLobotomyMod.Challenges
+{
+    public static class ApostleGrizzlies
+    {
+        public static AscensionChallenge Id { get; private set; }
+
+        public static void Register()
+        {
+            Id = ChallengeManager.Add(
+                LobotomyPlugin.pluginGuid,
+                "Apostle Grizzlies",
+                "Guardian Apostles appear during the first 3 boss battles.",
+                30,
+                TextureLoader.LoadTextureFromFile("ascensionApostleBear.png"),
+                TextureLoader.LoadTextureFromFile("ascensionApostleBear_activated.png")
+                )
+                .SetDependantChallengeGetterStatic(AscensionChallenge.GrizzlyMode)
+                .Challenge.challengeType;
+        }
+    }
+}
