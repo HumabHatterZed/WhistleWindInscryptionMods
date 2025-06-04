cf30e0c6e4353d715f1022c258412adaa1f38a8f
WhistleWind <humanhaterq@gmail.com>
2025-06-03 21:40:49 -0700

    Added Death Penalty sigil (Stub)

---
+++ WhistleWind.AbnormalSigils/Abilities/DeathPenalty.cs
@@ -0,0 +1,40 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Triggers;
+using System;
+using System.Collections;
+using UnityEngine;
+using WhistleWind.AbnormalSigils.Core.Helpers;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils
+{
+    public partial class AbnormalPlugin
+    {
+        private void Ability_DeathPenalty()
+        {
+            const string rulebookName = "Death Penalty";
+            const string rulebookDescription = "When [creature] dies, its owner takes 1 damage.";
+            const string dialogue = "Pay better care to your beasts.";
+            Soulbound.ability = AbnormalAbilityHelper.CreateAbility<DeathPenalty>(
+                "sigilDeathPenalty",
+                rulebookName, rulebookDescription, dialogue, powerLevel: -4,
+                modular: false, opponent: false, canStack: false)
+                .SetPart3Rulebook()
+                .SetGrimoraRulebook()
+                .SetMagnificusRulebook().Id;
+        }
+    }
+    public class DeathPenalty : AbilityBehaviour
+    {
+        public static Ability ability;
+        public override Ability Ability => ability;
+
+        public override bool RespondsToDie(bool wasSacrifice, PlayableCard killer) => !wasSacrifice;
+        public override IEnumerator OnDie(bool wasSacrifice, PlayableCard killer) {
+            yield return base.PreSuccessfulTriggerSequence();
+            yield return LifeManager.Instance.ShowDamageSequence(1, 1, !base.Card.OpponentCard, changeView: false);
+            yield return new WaitForSeconds(0.3f);
+            yield return base.LearnAbility(0.4f);
+        }
+    }
+}
