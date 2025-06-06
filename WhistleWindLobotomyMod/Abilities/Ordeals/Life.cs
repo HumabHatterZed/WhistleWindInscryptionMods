e19fd9c7a3c828facfda3f219bd369983bcad12f
WhistleWind <humanhaterq@gmail.com>
2025-06-06 12:04:35 -0700

    Reworked

--- WhistleWindLobotomyMod/Abilities/Ordeals/Life.cs
+++ WhistleWindLobotomyMod/Abilities/Ordeals/Life.cs
@@ -1,6 +1,10 @@
-﻿using DiskCardGame;
+﻿using Core.Helpers;
+using DiskCardGame;
 using InscryptionAPI.Card;
+using InscryptionAPI.Helpers.Extensions;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using WhistleWind.Core.Helpers;
 
@@ -12,39 +16,35 @@ namespace WhistleWindLobotomyMod
         {
             AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
             info.rulebookName = "Life";
-            info.rulebookDescription = "At the start of every other turn for the owner, this card creates a random Doubt or Process of Understanding in empty adjacent spaces.";
+            info.rulebookDescription = "When this card is played, create two random Dawn of Greens on the board. Two turns after being played, return to the queue.";
             info.powerLevel = 4;
             Life.ability = AbilityManager.Add(LobotomyPlugin.pluginGuid, info, typeof(Life), TextureLoader.LoadTextureFromFile("sigilLife.png")).Id;
         }
     }
 
-    public class Life : CreateCardsAdjacent
+    public class Life : AbilityBehaviour
     {
         public static Ability ability;
         public override Ability Ability => ability;
 
-        int turnsTillActivation = 2;
-        public override string SpawnedCardId => GetRandomCardId();
-
-        public override string CannotSpawnDialogue => "The pain is refused.";
-
-        public override bool RespondsToResolveOnBoard() => false;
-        public override bool RespondsToUpkeep(bool playerUpkeep) => base.Card.OpponentCard != playerUpkeep;
+        public override bool RespondsToResolveOnBoard() => true;
+        public override IEnumerator OnResolveOnBoard() {
+            int rand = base.GetRandomSeed();
+            yield return CombatHelpers.CreateCardInRandomSlot(CardLoader.GetCardByName(GetRandomCardId(rand++)), BoardManager.Instance.GetOpponentOpenSlots());
+            yield return CombatHelpers.CreateCardInRandomSlot(CardLoader.GetCardByName(GetRandomCardId(rand++)), BoardManager.Instance.GetOpponentOpenSlots());
+        }
+        public override bool RespondsToUpkeep(bool playerUpkeep) => base.Card.OpponentCard != playerUpkeep && TurnManager.Instance.TurnNumber > base.Card.TurnPlayed + 2;
         public override IEnumerator OnUpkeep(bool playerUpkeep)
         {
-            turnsTillActivation--;
-            if (turnsTillActivation == 0)
-            {
-                yield return base.OnResolveOnBoard();
-                turnsTillActivation = 2;
-            }
+            base.Card.Slot.Card = null;
+            base.Card.Slot = BoardManager.Instance.OpponentSlotsCopy.FindAll(x => !TurnManager.Instance.Opponent.QueuedSlots.Contains(x)).GetSeededRandom(base.GetRandomSeed());
+            yield return TurnManager.Instance.Opponent.ReturnCardToQueue(base.Card, 0.2f);
         }
-        private string GetRandomCardId()
+        private string GetRandomCardId(int randomSeed)
         {
             // .48 .66 .80 .92 1
             // .41 .57 .75 .89 1
             // .34 .48 .70 .86 1
-            int randomSeed = base.GetRandomSeed();
             float randomValue = SeededRandom.Value(randomSeed);
             int extraDifficulty = AscensionSaveData.Data.GetNumChallengesOfTypeActive(AscensionChallenge.BaseDifficulty);
             if (randomValue <= Mathf.Max(0.15f, 0.55f - extraDifficulty * 0.07f))
