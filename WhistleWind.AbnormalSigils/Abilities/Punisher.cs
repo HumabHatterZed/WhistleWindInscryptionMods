acbccbec84b503ed3403db953fcd2ee0a1032142
WhistleWind <humanhaterq@gmail.com>
2025-06-05 22:53:33 -0700

    nerfed to trigger on death instead

--- WhistleWind.AbnormalSigils/Abilities/Punisher.cs
+++ WhistleWind.AbnormalSigils/Abilities/Punisher.cs
@@ -12,12 +12,12 @@ namespace WhistleWind.AbnormalSigils
         private void Ability_Punisher()
         {
             const string rulebookName = "Punisher";
-            const string rulebookDescription = "When [creature] is struck, the striker is killed.";
+            const string rulebookDescription = "When [creature] is struck and killed, the striker is also killed.";
             const string dialogue = "Retaliation is switft and brutal.";
             const string triggerText = "[creature] swiftly retaliates!";
             Punisher.ability = AbnormalAbilityHelper.CreateAbility<Punisher>(
                 "sigilPunisher",
-                rulebookName, rulebookDescription, dialogue, triggerText, powerLevel: 4,
+                rulebookName, rulebookDescription, dialogue, triggerText, powerLevel: 3,
                 modular: false, opponent: true, canStack: false)
                 .SetPart3Rulebook()
                 .SetGrimoraRulebook()
@@ -28,19 +28,15 @@ namespace WhistleWind.AbnormalSigils
     {
         public static Ability ability;
         public override Ability Ability => ability;
-        public override bool RespondsToTakeDamage(PlayableCard source)
-        {
-            if (source != null && !source.Dead && source != base.Card)
-                return source.Health > 0 && !source.HasAbility(Ability.MadeOfStone) && !source.HasTrait(AbnormalPlugin.ImmuneToInstaDeath);
-
+        public override bool RespondsToDie(bool wasSacrifice, PlayableCard killer) {
+            if (!wasSacrifice && killer != null) {
+                return !killer.HasAbility(Ability.MadeOfStone) && !killer.HasTrait(AbnormalPlugin.ImmuneToInstaDeath);
+            }
             return false;
         }
-        public override IEnumerator OnTakeDamage(PlayableCard source)
-        {
+        public override IEnumerator OnDie(bool wasSacrifice, PlayableCard killer) {
             yield return base.PreSuccessfulTriggerSequence();
-            base.Card.Anim.StrongNegationEffect();
-            yield return new WaitForSeconds(0.55f);
-            yield return source.Die(false, base.Card);
+            yield return killer.Die(false, base.Card);
             yield return base.LearnAbility(0.4f);
         }
     }
