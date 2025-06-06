1581c7b7c6b64f1626cb775c19b4b11f0637baea
WhistleWind <humanhaterq@gmail.com>
2025-06-05 22:55:22 -0700

    Added slot prioritising to QueueCreatedCard, added documentation, fixed a couple things

--- Core/Helpers/CombatHelpers.cs
+++ Core/Helpers/CombatHelpers.cs
@@ -10,7 +10,12 @@ namespace Core.Helpers
 {
     public static class CombatHelpers
     {
-        public static IEnumerator QueueCreatedCard(CardInfo cardToQueue)
+        /// <summary>
+        /// Creates a card with the given CardInfo in the opponent queue.
+        /// </summary>
+        /// <param name="cardToQueue">CardInfo for the card to add to the opponent queue.</param>
+        /// <param name="slot">The queue slot to add this card to. If null, select a random open slot.</param>
+        public static IEnumerator QueueCreatedCard(CardInfo cardToQueue, CardSlot slot = null)
         {
             int randomSeed = SaveManager.SaveFile.GetCurrentRandomSeed();
             List<CardSlot> openSlots = BoardManager.Instance.OpponentSlotsCopy.Where(x => !TurnManager.Instance.Opponent.QueuedSlots.Contains(x)).ToList();
@@ -24,13 +29,24 @@ namespace Core.Helpers
             }
             else
             {
-                CardSlot index = openSlots[SeededRandom.Range(0, openSlots.Count, randomSeed++)];
+                CardSlot index = slot ?? openSlots[SeededRandom.Range(0, openSlots.Count, randomSeed++)];
                 ViewManager.Instance.SwitchToView(View.OpponentQueue);
+                PlayableCard playableCard = TurnManager.Instance.Opponent.Queue.Find(x => x.QueuedSlot == index);
+                if (playableCard != null) {
+                    playableCard.ExitBoard(0.25f, new Vector3(-1f, -2f, 5f));
+                    TurnManager.Instance.Opponent.Queue.Remove(playableCard);
+                }
                 yield return Singleton<TurnManager>.Instance.Opponent.QueueCard(cardToQueue, index);
             }
             yield return new WaitForSeconds(0.45f);
         }
 
+        /// <summary>
+        /// Creates a card with the given CardInfo in a random slot select from the given list.
+        /// If the list of valid slots is empty, queue the created card.
+        /// </summary>
+        /// <param name="info">The CardInfo the created card will have.</param>
+        /// <param name="validSlots">List of CardSlots that the created card can be assigned to.</param>
         public static IEnumerator CreateCardInRandomSlot(CardInfo info, List<CardSlot> validSlots)
         {
             if (validSlots.Count > 0)
