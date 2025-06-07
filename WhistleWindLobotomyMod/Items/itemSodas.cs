1198b2610f78962037a626c9d757cb6c42cfe62e
WhistleWind <humanhaterq@gmail.com>
2025-06-07 16:24:32 -0700

    Fixed duplicate text

--- WhistleWindLobotomyMod/Items/itemSodas.cs
+++ WhistleWindLobotomyMod/Items/itemSodas.cs
@@ -18,7 +18,7 @@ namespace WhistleWindLobotomyMod
             GameObject obj4 = AssetManager.AssetBundle.LoadAsset<GameObject>("SurefireDrink");
             ConsumableItemManager.New(LobotomyPlugin.pluginGuid,
                 "Fizzy Lifting Drink",
-                "To the user: Select a card on the board to gain Airborne for 3 turns.", TextureLoader.LoadTextureFromFile("itemFizzyLifter.png", LobotomyPlugin.ModAssembly), typeof(FizzyLifterItem), obj)
+                "Select a card on the board to gain Airborne for 3 turns.", TextureLoader.LoadTextureFromFile("itemFizzyLifter.png", LobotomyPlugin.ModAssembly), typeof(FizzyLifterItem), obj)
                 .SetPowerLevel(1)
                 .SetPlacedSoundId("can_hit")
                 .SetPickupSoundId("can_hit")
@@ -26,7 +26,7 @@ namespace WhistleWindLobotomyMod
 
             ConsumableItemManager.New(LobotomyPlugin.pluginGuid,
                 "Ocean Flavoured Soda",
-                "To the user: Select a card on the board to gain Waterborne for 3 turns.", TextureLoader.LoadTextureFromFile("itemOceanSoda.png", LobotomyPlugin.ModAssembly), typeof(OceanSodaItem), obj2)
+                "Select a card on the board to gain Waterborne for 3 turns.", TextureLoader.LoadTextureFromFile("itemOceanSoda.png", LobotomyPlugin.ModAssembly), typeof(OceanSodaItem), obj2)
                 .SetPowerLevel(1)
                 .SetPlacedSoundId("can_hit")
                 .SetPickupSoundId("can_hit")
@@ -34,7 +34,7 @@ namespace WhistleWindLobotomyMod
 
             ConsumableItemData item = ConsumableItemManager.New(LobotomyPlugin.pluginGuid,
                 "Potshot Pop",
-                "To the user: Select a card on the board to gain Sentry for this and next turn.", TextureLoader.LoadTextureFromFile("itemPotshotPop.png", LobotomyPlugin.ModAssembly), typeof(PotshotPopItem), obj3)
+                "Select a card on the board to gain Sentry for this and next turn.", TextureLoader.LoadTextureFromFile("itemPotshotPop.png", LobotomyPlugin.ModAssembly), typeof(PotshotPopItem), obj3)
                 .SetPowerLevel(2)
                 .SetPlacedSoundId("can_hit")
                 .SetPickupSoundId("can_hit")
@@ -42,7 +42,7 @@ namespace WhistleWindLobotomyMod
 
             ConsumableItemData item2 = ConsumableItemManager.New(LobotomyPlugin.pluginGuid,
                 "Surefire Soda",
-                "To the user: Select a card on the board to gain Sniper for this turn.", TextureLoader.LoadTextureFromFile("itemSurefireDrink.png", LobotomyPlugin.ModAssembly), typeof(SurefireDrinkItem), obj4)
+                "Select a card on the board to gain Sniper for this turn.", TextureLoader.LoadTextureFromFile("itemSurefireDrink.png", LobotomyPlugin.ModAssembly), typeof(SurefireDrinkItem), obj4)
                 .SetPlacedSoundId("can_hit")
                 .SetPickupSoundId("can_hit")
                 .SetPowerLevel(4)
