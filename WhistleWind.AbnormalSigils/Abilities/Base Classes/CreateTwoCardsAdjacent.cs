beed488dd93f90ce29ad8e2eb76111f452508021
WhistleWind <humanhaterq@gmail.com>
2025-06-27 12:40:20 -0700

    New base class

---
+++ WhistleWind.AbnormalSigils/Abilities/Base Classes/CreateTwoCardsAdjacent.cs
@@ -0,0 +1,61 @@
+﻿using DiskCardGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhistleWind.AbnormalSigils {
+    /// <summary>
+    /// Version of vanilla CreateCardsAdjacent class where the left and right card IDs are defined separately.
+    /// </summary>
+    public abstract class CreateTwoCardsAdjacent : AbilityBehaviour {
+        protected abstract string LeftSpawnedCardId { get; }
+        protected abstract string RightSpawnedCardId { get; }
+        protected abstract string CannotSpawnDialogue { get; }
+
+        public override bool RespondsToResolveOnBoard() => true;
+        public override IEnumerator OnResolveOnBoard() {
+            Singleton<ViewManager>.Instance.SwitchToView(View.Board);
+            CardSlot toLeft = Singleton<BoardManager>.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: true);
+            CardSlot toRight = Singleton<BoardManager>.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: false);
+            bool toLeftValid = toLeft != null && toLeft.Card == null;
+            bool toRightValid = toRight != null && toRight.Card == null;
+            yield return base.PreSuccessfulTriggerSequence();
+            if (toLeftValid) {
+                yield return new WaitForSeconds(0.1f);
+                yield return this.SpawnCardOnSlot(toLeft, LeftSpawnedCardId);
+            }
+            if (toRightValid) {
+                yield return new WaitForSeconds(0.1f);
+                yield return this.SpawnCardOnSlot(toRight, RightSpawnedCardId);
+            }
+            if (toLeftValid || toRightValid) {
+                yield return base.LearnAbility();
+            }
+            else if (!base.HasLearned) {
+                yield return Singleton<TextDisplayer>.Instance.ShowUntilInput(this.CannotSpawnDialogue, -0.65f, 0.4f);
+            }
+        }
+
+        private IEnumerator SpawnCardOnSlot(CardSlot slot, string alternateSpawnCardId) {
+            CardInfo cardByName = CardLoader.GetCardByName(alternateSpawnCardId);
+            this.ModifySpawnedCard(cardByName);
+            yield return Singleton<BoardManager>.Instance.CreateCardInSlot(cardByName, slot, 0.15f);
+        }
+
+        private void ModifySpawnedCard(CardInfo card) {
+            List<Ability> abilities = base.Card.Info.Abilities;
+            foreach (CardModificationInfo temporaryMod in base.Card.TemporaryMods) {
+                abilities.AddRange(temporaryMod.abilities);
+            }
+            abilities.RemoveAll(x => x == this.Ability);
+            if (abilities.Count > 4) {
+                abilities.RemoveRange(3, abilities.Count - 4);
+            }
+            CardModificationInfo cardModificationInfo = new() {
+                fromCardMerge = true,
+                abilities = abilities
+            };
+            card.Mods.Add(cardModificationInfo);
+        }
+    }
+}
