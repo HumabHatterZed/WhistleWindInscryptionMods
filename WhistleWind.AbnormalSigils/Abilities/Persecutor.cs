b2a1014d7248f945b19bc6b59b1420b99af3fca4
WhistleWind <humanhaterq@gmail.com>
2025-06-27 12:40:37 -0700

    Refactored to use new base class, changed desc and added XML

--- WhistleWind.AbnormalSigils/Abilities/Persecutor.cs
+++ WhistleWind.AbnormalSigils/Abilities/Persecutor.cs
@@ -1,7 +1,4 @@
 ﻿using DiskCardGame;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using WhistleWind.AbnormalSigils.Core.Helpers;
 using WhistleWind.Core.Helpers;
 
@@ -9,7 +6,7 @@ namespace WhistleWind.AbnormalSigils {
     public partial class AbnormalPlugin {
         private void Ability_Persecutor() {
             const string rulebookName = "Persecutor";
-            const string rulebookDescription = "When [creature] is played, create a Nail in the adjacent left space and a Hammer in the adjacent right space if they are empty.";
+            const string rulebookDescription = "When [creature] is played, create a Nail and Hammer in the adjacent left and right spaces respectively if they are empty.";
             const string dialogue = "Are you guilty of having a closed heart?";
             const string triggerText = "[creature] reveals its hidden tools!";
             Persecutor.ability = AbnormalAbilityHelper.CreateAbility<Persecutor>(
@@ -21,57 +18,15 @@ namespace WhistleWind.AbnormalSigils {
                 .SetMagnificusRulebook().Id;
         }
     }
-    public class Persecutor : AbilityBehaviour {
+    /// <summary>
+    /// When [creature] is played, create a Nail and Hammer in the adjacent left and right spaces respectively if they are empty.
+    /// </summary>
+    public class Persecutor : CreateTwoCardsAdjacent {
         public static Ability ability;
         public override Ability Ability => ability;
 
-        private readonly string CannotSpawnDialogue = "These tools remain hidden for now.";
-
-        public override bool RespondsToResolveOnBoard() => true;
-
-        public override IEnumerator OnResolveOnBoard() {
-            Singleton<ViewManager>.Instance.SwitchToView(View.Board);
-            CardSlot toLeft = Singleton<BoardManager>.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: true);
-            CardSlot toRight = Singleton<BoardManager>.Instance.GetAdjacent(base.Card.Slot, adjacentOnLeft: false);
-            bool toLeftValid = toLeft != null && toLeft.Card == null;
-            bool toRightValid = toRight != null && toRight.Card == null;
-            yield return base.PreSuccessfulTriggerSequence();
-            if (toLeftValid) {
-                yield return new WaitForSeconds(0.1f);
-                yield return this.SpawnCardOnSlot(toLeft, true);
-            }
-            if (toRightValid) {
-                yield return new WaitForSeconds(0.1f);
-                yield return this.SpawnCardOnSlot(toRight, false);
-            }
-
-            if (toLeftValid || toRightValid)
-                yield return base.LearnAbility();
-
-            else if (!base.HasLearned)
-                yield return Singleton<TextDisplayer>.Instance.ShowUntilInput(this.CannotSpawnDialogue, -0.65f, 0.4f);
-        }
-
-        private IEnumerator SpawnCardOnSlot(CardSlot slot, bool left) {
-            CardInfo cardByName = CardLoader.GetCardByName(left ? "wstl_nail" : "wstl_hammer");
-            this.ModifySpawnedCard(cardByName);
-            yield return Singleton<BoardManager>.Instance.CreateCardInSlot(cardByName, slot, 0.15f);
-        }
-
-        private void ModifySpawnedCard(CardInfo card) {
-            List<Ability> abilities = base.Card.Info.Abilities;
-            foreach (CardModificationInfo temporaryMod in base.Card.TemporaryMods)
-                abilities.AddRange(temporaryMod.abilities);
-
-            abilities.RemoveAll((Ability x) => x == this.Ability);
-            if (abilities.Count > 4)
-                abilities.RemoveRange(3, abilities.Count - 4);
-
-            CardModificationInfo cardModificationInfo = new() {
-                fromCardMerge = true,
-                abilities = abilities
-            };
-            card.Mods.Add(cardModificationInfo);
-        }
+        protected override string LeftSpawnedCardId => "wstl_nail";
+        protected override string RightSpawnedCardId => "wstl_hammer";
+        protected override string CannotSpawnDialogue => "These tools remain hidden for now.";
     }
 }
