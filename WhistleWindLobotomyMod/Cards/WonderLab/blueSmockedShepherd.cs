9e7a1fb507c29679dd5950b6e213ad0077c1e5d7
WhistleWind <humanhaterq@gmail.com>
2025-06-28 22:36:21 -0700

    smock man

--- WhistleWindLobotomyMod/Cards/WonderLab/blueSmockedShepherd.cs
+++ WhistleWindLobotomyMod/Cards/WonderLab/blueSmockedShepherd.cs
@@ -1,18 +1,20 @@
-﻿using InscryptionAPI.Card;
+﻿using DiskCardGame;
+using InscryptionAPI.Card;
 using WhistleWind.AbnormalSigils;
 using WhistleWind.Core.Helpers;
 using static WhistleWindLobotomyMod.Core.LobotomyCardManager;
 
 namespace WhistleWindLobotomyMod {
     public partial class Cards {
-        public const string blueSmockedShepherd = "wstl_blueSmockedShepherd";
+        public const string blueSmockedShepherd = "wstlWonder_blueSmockedShepherd";
         private static void BlueSmockedShepherd() {
-            return;
-            string textureName = "blueSmockedShepherd";
+            string textureName = "blueShepherd";
             CardManager.New(LobotomyPlugin.wonderlabPrefix, blueSmockedShepherd, "Blue-Smocked Shepherd",
-                attack: 0, health: 0)
+                attack: 2, health: 2, "A cruel shepherd with a penchant for lying.")
+                .SetBonesCost(5)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
-                .AddAbilities(Abusive.ability)
+                .AddAbilities(Abusive.ability, Ability.SplitStrike)
+                .AddTribes(AbnormalPlugin.TribeAnthropoid)
                 .Build(CardHelper.CardType.Common, RiskLevel.He, true);
         }
     }
