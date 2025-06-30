93d3e2d12ab9839e5dc7e4961260a96534cabc04
WhistleWind <humanhaterq@gmail.com>
2025-06-29 21:44:06 -0700

    condense code

--- WhistleWind.AbnormalSigils/StatusEffects/StatusEffectBehaviour.cs
+++ WhistleWind.AbnormalSigils/StatusEffects/StatusEffectBehaviour.cs
@@ -10,7 +10,7 @@ namespace WhistleWind.AbnormalSigils.StatusEffects {
         public const string _DECAL = "_decal";
         public const string STATUS_ = "status_";
 
-        private int _priority = int.MaxValue;
+        private int _priority = int.MinValue;
         public override int Priority => _priority;
 
         public int TurnGained = -1;
@@ -32,43 +32,32 @@ namespace WhistleWind.AbnormalSigils.StatusEffects {
         public virtual bool RespondsToStatusEffectRemoved(PlayableCard target, StatusEffectBehaviour statusEffect) {
             return false;
         }
-
         public virtual IEnumerator OnStatusEffectRemoved(PlayableCard target, StatusEffectBehaviour statusEffect) {
             yield break;
         }
 
         public void ModifyPotency(int amount, bool updateDecals) {
             EffectPotency += amount;
+            UpdateStatusEffectMods(updateDecals);
+        }
+        public void SetPotency(int amount, bool updateDecals) {
+            EffectPotency = amount;
+            UpdateStatusEffectMods(updateDecals);
+        }
 
+        public void UpdateStatusEffectMods(bool updateDecals) {
             CardModificationInfo mod = GetStatusPotencyMod(false);
             base.PlayableCard.RemoveTemporaryMod(mod, false);
-            if (EffectPotency > 0)
-                base.PlayableCard.AddTemporaryMod(GetStatusPotencyMod(true));
+
+            if (EffectPotency > 0) {
+                mod = GetStatusPotencyMod(true);
+                base.PlayableCard.AddTemporaryMod(mod);
+            }
             else
                 base.PlayableCard.OnStatsChanged();
 
             if (updateDecals) {
                 CardModificationInfo mod2 = GetStatusDecalsMod(false);
-                base.PlayableCard.RemoveTemporaryMod(mod2, false);
-                if (EffectDecalIds().Count > 0)
-                    base.PlayableCard.AddTemporaryMod(GetStatusDecalsMod(true));
-                else
-                    base.PlayableCard.OnStatsChanged();
-            }
-        }
-        public void SetPotency(int amount, bool updateDecals) {
-            EffectPotency = amount;
-
-            CardModificationInfo mod = GetStatusPotencyMod(false);
-            base.PlayableCard.RemoveTemporaryMod(mod, false);
-
-            if (EffectPotency > 0)
-                base.PlayableCard.AddTemporaryMod(GetStatusPotencyMod(true));
-            else
-                base.PlayableCard.OnStatsChanged();
-
-            if (updateDecals) {
-                CardModificationInfo mod2 = base.PlayableCard.TemporaryMods.Find(x => x.singletonId == ModSingletonName + _DECAL);
                 base.PlayableCard.RemoveTemporaryMod(mod2, false);
                 if (EffectDecalIds().Count > 0)
                     base.PlayableCard.AddTemporaryMod(GetStatusDecalsMod(true));
@@ -85,9 +74,11 @@ namespace WhistleWind.AbnormalSigils.StatusEffects {
                     nonCopyable = !EffectCanBeInherited
                 };
                 retval.specialAbilities.Add(StatusEffect);
+                for (int i = 0; i < EffectPotency; i++) {
+                    retval.abilities.Add(IconAbility);
+                }
+
                 retval.SetStatusEffect();
-                for (int i = 0; i < EffectPotency; i++)
-                    retval.AddAbilities(IconAbility);
             }
             else {
                 retval = base.PlayableCard.TemporaryMods.Find(x => x.singletonId == ModSingletonName);
@@ -106,13 +97,14 @@ namespace WhistleWind.AbnormalSigils.StatusEffects {
                 retval.decalIds = EffectDecalIds();
                 retval.SetStatusEffect();
             }
-            retval = base.PlayableCard.TemporaryMods.Find(x => x.singletonId == (ModSingletonName + _DECAL));
+            else {
+                retval = base.PlayableCard.TemporaryMods.Find(x => x.singletonId == (ModSingletonName + _DECAL));
+            }
             return retval;
         }
 
         public IEnumerator RemoveFromCard(bool triggerOnRemoved, bool updateDisplay = true) {
-            List<CardModificationInfo> mods = base.PlayableCard.TemporaryMods.Where(x => x.specialAbilities.Contains(this.StatusEffect)).ToList();
-            foreach (CardModificationInfo mod in mods) {
+            foreach (CardModificationInfo mod in base.PlayableCard.TemporaryMods.Where(x => x.specialAbilities.Contains(this.StatusEffect))) {
                 base.PlayableCard.RemoveTemporaryMod(mod, false);
             }
             if (updateDisplay)
