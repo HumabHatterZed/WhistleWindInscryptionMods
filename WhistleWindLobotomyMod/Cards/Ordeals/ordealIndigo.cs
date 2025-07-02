88d7bda7ed3e5c4a139245e4675e49b4c88f3c5c
WhistleWind <humanhaterq@gmail.com>
2025-07-02 00:07:46 -0700

    split sweepers in trice twain

--- WhistleWindLobotomyMod/Cards/Ordeals/ordealIndigo.cs
+++ WhistleWindLobotomyMod/Cards/Ordeals/ordealIndigo.cs
@@ -9,14 +9,38 @@ using static WhistleWindLobotomyMod.Core.LobotomyCardManager;
 namespace WhistleWindLobotomyMod {
     public partial class Cards {
         public const string sweeper = "wstl_sweeper";
+        public const string sweeper2 = "wstl_sweeper2";
+        public const string sweeper3 = "wstl_sweeper3";
         private static void Cards_IndigoOrdeal() {
             string textureName = "sweeper";
-            CardInfo sweeperCard = CardManager.New(LobotomyPlugin.pluginPrefix, sweeper, "Sweeper",
-                attack: 2, health: 3)
+            string textureName2 = "sweeper2";
+            string textureName3 = "sweeper3";
+            CardManager.New(LobotomyPlugin.pluginPrefix, sweeper, "Sweeper A",
+                attack: 1, health: 2)
                 .SetBloodCost(2).SetEnergyCost(2)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
-                .AddAbilities(Persistent.ability, Bloodfiend.ability)
-                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission, SweeperAppearance.appearance)
+                .AddAbilities(Ability.SplitStrike, Persistent.ability, Bloodfiend.ability)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
+                .AddTribes(TribeFae)
+                .AddTraits(Ordeal)
+                .Build();
+
+            CardManager.New(LobotomyPlugin.pluginPrefix, sweeper2, "Sweeper B",
+                attack: 1, health: 2)
+                .SetBloodCost(2).SetEnergyCost(2)
+                .SetPortraits(LobotomyPlugin.ModAssembly, textureName2)
+                .AddAbilities(Ability.DoubleStrike, Persistent.ability, Bloodfiend.ability)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
+                .AddTribes(TribeFae)
+                .AddTraits(Ordeal)
+                .Build();
+
+            CardManager.New(LobotomyPlugin.pluginPrefix, sweeper3, "Sweeper C",
+                attack: 2, health: 2)
+                .SetBloodCost(2).SetEnergyCost(2)
+                .SetPortraits(LobotomyPlugin.ModAssembly, textureName3)
+                .AddAbilities(Ability.Sniper, Persistent.ability, Bloodfiend.ability)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
                 .AddTribes(TribeFae)
                 .AddTraits(Ordeal)
                 .Build();
