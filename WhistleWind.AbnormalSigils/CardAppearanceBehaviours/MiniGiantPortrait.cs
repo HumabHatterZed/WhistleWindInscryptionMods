efcd47a8454d9ca5a298d7ff9b6fda26d23902db
WhistleWind <humanhaterq@gmail.com>
2025-07-04 11:44:08 -0700

    fix mini giant not having live render layer

--- WhistleWind.AbnormalSigils/CardAppearanceBehaviours/MiniGiantPortrait.cs
+++ WhistleWind.AbnormalSigils/CardAppearanceBehaviours/MiniGiantPortrait.cs
@@ -12,13 +12,28 @@ namespace WhistleWind.AbnormalSigils {
         public static Appearance appearance;
 
         public override void ApplyAppearance() {
+            // can't call LiveRenderCard.ApplyAppearance cause it crashes for some reason
+            RenderStatsLayer statsLayer = base.Card.StatsLayer;
+            if (statsLayer is not RenderLiveStatsLayer) {
+                GameObject statsLayerObj = statsLayer.gameObject;
+                statsLayer.PreserveMaterialOnDestroy = true;
+                Object.DestroyImmediate(statsLayer);
+                RenderLiveStatsLayer renderLiveStatsLayer = this.AddStatsLayerComponent(statsLayerObj);
+                renderLiveStatsLayer.FindBendableRenderer();
+                SetMaterialTiling[] componentsInChildren = renderLiveStatsLayer.GetComponentsInChildren<SetMaterialTiling>(includeInactive: true);
+                foreach (SetMaterialTiling setMaterialTiling in componentsInChildren) {
+                    if (setMaterialTiling.MaterialIndex == 0) {
+                        setMaterialTiling.AdjustOffset(new Vector2(2f, 1f));
+                    }
+                }
+            }
+
             base.Card.RenderInfo.prefabPortrait = base.Card.Info.AnimatedPortrait;
             base.Card.RenderInfo.hidePortrait = true;
         }
 
         public override RenderLiveStatsLayer AddStatsLayerComponent(GameObject statsLayerObj) {
-            RenderLiveStatsLayer renderLiveStatsLayer = statsLayerObj.AddComponent<RenderLiveStatsLayer>();
-            return renderLiveStatsLayer;
+            return statsLayerObj.AddComponent<RenderLiveStatsLayer>();
         }
     }
 }
