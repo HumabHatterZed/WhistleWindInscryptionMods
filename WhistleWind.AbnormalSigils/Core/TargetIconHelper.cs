06575c1b33d1dcba104b33b4659d7496b8f64182
WhistleWind <humanhaterq@gmail.com>
2025-07-03 17:15:43 -0700

    Support for specifying own target prefab

--- WhistleWind.AbnormalSigils/Core/TargetIconHelper.cs
+++ WhistleWind.AbnormalSigils/Core/TargetIconHelper.cs
@@ -3,9 +3,22 @@ using Pixelplacement;
 using UnityEngine;
 
 namespace WhistleWind.AbnormalSigils.Core {
+    /// <summary>
+    /// Contains methods for creating and cleaning up target icon GameObjects.
+    /// </summary>
     public class TargetIconHelper {
         public static GameObject targetIconPrefab = ResourceBank.Get<GameObject>("Prefabs/Cards/SpecificCardModels/CannonTargetIcon");
 
+        public static GameObject CreateTargetIcon(CardSlot targetSlot, GameObject targetPrefab, Color materialColour = default) {
+            GameObject gameObject = GameObject.Instantiate(targetPrefab, targetSlot.transform);
+            gameObject.transform.localPosition = new Vector3(0f, 0.25f, 0f);
+            gameObject.transform.localRotation = Quaternion.identity;
+
+            if (materialColour != default)
+                gameObject.GetComponentInChildren<MeshRenderer>().material.color = materialColour;
+
+            return gameObject;
+        }
         public static GameObject CreateTargetIcon(CardSlot targetSlot, Color materialColour = default) {
             GameObject gameObject = GameObject.Instantiate(targetIconPrefab, targetSlot.transform);
             gameObject.transform.localPosition = new Vector3(0f, 0.25f, 0f);
