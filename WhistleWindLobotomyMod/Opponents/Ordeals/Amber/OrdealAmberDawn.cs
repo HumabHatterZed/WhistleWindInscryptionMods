6e46b864cf94a724a39d3e61fe40fbc28340e548
WhistleWind <humanhaterq@gmail.com>
2025-07-04 21:54:20 -0700

    fixed blueprints

--- WhistleWindLobotomyMod/Opponents/Ordeals/Amber/OrdealAmberDawn.cs
+++ WhistleWindLobotomyMod/Opponents/Ordeals/Amber/OrdealAmberDawn.cs
@@ -4,52 +4,36 @@ using System.Collections.Generic;
 
 namespace WhistleWindLobotomyMod.Opponents {
     /// <summary>
-    /// Appears in R0
-    /// Difficulty range: (1 - 3) +[0,2] // 4 difficulty is for boss node w/o challlenges
-    /// 
-    /// D | Turn 1 | Turn 2 | Turn 3 | Turn 4 | ## | HP | Atk
-    /// 1 | P      | P P    | P      | P P    | 6  | 6  | 6
-    /// 2 | P P    | P P    | P P    | P P    | 8  | 8  | 8
-    /// 3 | P P    | P P P  | P P    | P P P  | 10 | 10 | 10
-    /// 4 | P P P  | P P P  | P P P  | P P P  | 12 | 12 | 12
-    /// 5 | P P P  | P P P P| P P P  | P P P P| 14 | 14 | 14
+    /// The weakest Amber Ordeal.
+    /// Crimson Ordeals are themed around constantly spawning enemies.
+    /// Dawn will have cards appear one after the other with no downtime.
+    /// Cards required: 4, 6, 9,...
+    /// Valid regions: 0, 1
     /// </summary>
     public class OrdealAmberDawn : OrdealBattleSequencer {
         public override int ConstructOrdealBlueprint(EncounterData encounterData, int difficulty) {
-            List<EncounterBlueprintData.CardBlueprint> turn1 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-            List<EncounterBlueprintData.CardBlueprint> turn2 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood),
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-            List<EncounterBlueprintData.CardBlueprint> turn3 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-            List<EncounterBlueprintData.CardBlueprint> turn4 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood),
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-
-            for (int i = 2; i < encounterData.Difficulty + 1; i++) {
-                if (i % 2 == 0) {
-                    turn1.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                    turn3.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                }
-                else {
-                    turn2.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                    turn4.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                }
-
-                if (turn1.Count >= 4 && turn2.Count >= 4) break;
+            int num = 0;
+            int numTurns = 3;
+            if (encounterData.Difficulty > 3) {
+                numTurns += 1 + (encounterData.Difficulty - 3) / 2;
             }
 
-            encounterData.Blueprint.AddTurns(turn1, turn2, turn3, turn4);
-            return -1;
+            for (int i = 0; i < numTurns; i++) {
+                List<EncounterBlueprintData.CardBlueprint> turn = new();
+                if (i % 3 == 0) {
+                    turn.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
+                    num++;
+                }
+                if (i > 2 && i % 2 == 0) {
+                    turn.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
+                    num++;
+                }
+                turn.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
+                num++;
+
+                encounterData.Blueprint.AddTurn(turn);
+            }
+            return num;
         }
     }
 }
--- WhistleWindLobotomyMod/Opponents/Ordeals/Amber/OrdealAmberDusk.cs
+++ WhistleWindLobotomyMod/Opponents/Ordeals/Amber/OrdealAmberDusk.cs
@@ -1,55 +1,56 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Encounters;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
+using WhistleWind.AbnormalSigils;
 
 namespace WhistleWindLobotomyMod.Opponents {
     /// <summary>
-    /// Appears in R0
-    /// Difficulty range: (1 - 3) +[0,2] // 4 difficulty is for boss node w/o challlenges
-    /// 
-    /// D | Turn 1 | Turn 2 | Turn 3 | Turn 4 | ## | HP | Atk
-    /// 1 | P      | P P    | P      | P P    | 6  | 6  | 6
-    /// 2 | P P    | P P    | P P    | P P    | 8  | 8  | 8
-    /// 3 | P P    | P P P  | P P    | P P P  | 10 | 10 | 10
-    /// 4 | P P P  | P P P  | P P P  | P P P  | 12 | 12 | 12
-    /// 5 | P P P  | P P P P| P P P  | P P P P| 14 | 14 | 14
+    /// The weakest Amber Ordeal.
+    /// Crimson Ordeals are themed around constantly spawning enemies.
+    /// Dusk cards will summon Dawn cards, which will be added to the total required.
+    /// Cards required: 3, 4, 5... (not counting Dawn cards)
+    /// Valid regions: 1, 2
     /// </summary>
     public class OrdealAmberDusk : OrdealBattleSequencer {
         public override int ConstructOrdealBlueprint(EncounterData encounterData, int difficulty) {
-            List<EncounterBlueprintData.CardBlueprint> turn1 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-            List<EncounterBlueprintData.CardBlueprint> turn2 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood),
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-            List<EncounterBlueprintData.CardBlueprint> turn3 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-            List<EncounterBlueprintData.CardBlueprint> turn4 = new()
-            {
-                EncounterManager.NewCardBlueprint(Cards.perfectFood),
-                EncounterManager.NewCardBlueprint(Cards.perfectFood)
-            };
-
-            for (int i = 2; i < encounterData.Difficulty + 1; i++) {
-                if (i % 2 == 0) {
-                    turn1.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                    turn3.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                }
-                else {
-                    turn2.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                    turn4.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
-                }
-
-                if (turn1.Count >= 4 && turn2.Count >= 4) break;
+            int num = 3;
+            if (RunState.Run.regionTier > 1) {
+                num++;
             }
-
-            encounterData.Blueprint.AddTurns(turn1, turn2, turn3, turn4);
-            return -1;
+            if (encounterData.Difficulty > 14) {
+                num++;
+            }
+            for (int i = 0; i < num; i++) {
+                List<EncounterBlueprintData.CardBlueprint> turn = new() {
+                    EncounterManager.NewCardBlueprint(Cards.foodChain)
+                };
+                if (i > 2 && i % 2 != 0) {
+                    turn.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
+                }
+                if (encounterData.Difficulty > 11) {
+                    turn.Add(EncounterManager.NewCardBlueprint(Cards.perfectFood));
+                }
+                encounterData.Blueprint.AddTurn(turn).AddTurn();
+                if (i % 3 == 0) {
+                    encounterData.Blueprint.AddTurn();
+                }
+            }
+            return num;
+        }
+        public override void ModifySpawnedCard(PlayableCard card) {
+            base.ModifySpawnedCard(card);
+            if (card.Info.name == Cards.perfectFood) {
+                amountKilledThisTurn--;
+            }
+        }
+        public override void ModifyQueuedCard(PlayableCard card) {
+            base.ModifyQueuedCard(card);
+            if (card.Info.name == Cards.perfectFood) {
+                amountKilledThisTurn--;
+            }
         }
     }
 }
