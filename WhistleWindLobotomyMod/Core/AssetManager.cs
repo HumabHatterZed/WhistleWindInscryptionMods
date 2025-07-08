33510cbe78a8506ee5c07719b00597ec6b423ef8
WhistleWind <humanhaterq@gmail.com>
2025-07-08 00:11:36 -0700

    twisted building asset

--- WhistleWindLobotomyMod/Core/AssetManager.cs
+++ WhistleWindLobotomyMod/Core/AssetManager.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using DiskCardGame;
+using HarmonyLib;
+using InscryptionAPI.Resource;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,6 +21,10 @@ namespace WhistleWindLobotomyMod.Core {
         public static readonly List<AudioClip> sfxClips = new();
         public static readonly List<AudioClip> musicLoops = new();
 
+        public static readonly List<GameObject> scenery = new();
+
+        public static readonly Dictionary<string, List<SceneryData>> CustomSceneryData = new();
+
         internal static void Initialise() {
             using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("WhistleWindLobotomyMod.lobmodbosses")) {
                 BossBundle = AssetBundle.LoadFromStream(stream);
@@ -33,6 +39,22 @@ namespace WhistleWindLobotomyMod.Core {
             ordealBannerPrefab = AssetBundle.LoadAsset<GameObject>("OrdealBanner");
             sfxClips.Add(AssetBundle.LoadAsset<AudioClip>("soda_open"));
             sfxClips.Add(AssetBundle.LoadAsset<AudioClip>("can_hit"));
+
+            GameObject obj = AssetBundle.LoadAsset<GameObject>("twisted_building");
+            MapElement elem = obj.AddComponent<MapElement>();
+            elem.Data = new MapElementData();
+
+            ResourceBankManager.Add(LobotomyPlugin.pluginGuid, SceneryData.PREFABS_ROOT + "twisted_building", obj);
+            List<SceneryData> twistedBuildings = new();
+            twistedBuildings.Add(ScriptableObject.CreateInstance<SceneryData>());
+            twistedBuildings[0].prefabNames = new() { "twisted_building" };
+            twistedBuildings[0].radius = 0.095f;
+            twistedBuildings[0].minScale = new(6f, 6f);
+            twistedBuildings[0].maxScale = new(8f, 8f);
+            twistedBuildings[0].baseEulers = new(266f, 0f, 0f);
+
+            CustomSceneryData.Add("twisted_building", twistedBuildings);
+
             LobOpponentUtils.InitBossObjects();
             OrdealUtils.InitOrdeals();
         }
