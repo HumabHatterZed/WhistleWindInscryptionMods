4ff9f36a04ace044e40c0c70292cbdb86c6aff64
WhistleWind <humanhaterq@gmail.com>
2025-07-09 14:33:17 -0700

    mechanical tribe

--- WhistleWindLobotomyMod/Cards/Lobotomy Corporation/mhz176.cs
+++ WhistleWindLobotomyMod/Cards/Lobotomy Corporation/mhz176.cs
@@ -1,11 +1,15 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Card;
+using WhistleWind.AbnormalSigils;
 using WhistleWind.Core.Helpers;
 
 using static WhistleWindLobotomyMod.Core.LobotomyCardManager;
 
 namespace WhistleWindLobotomyMod {
     public partial class Cards {
+        /// <summary>
+        /// 2/1 Annoying
+        /// </summary>
         public const string mhz176 = "wstl_mhz176";
         private static void MHz176_T0727() {
             string name = "1.76 MHz";
@@ -17,6 +21,7 @@ namespace WhistleWindLobotomyMod {
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
                 .AddAbilities(Ability.BuffEnemy)
                 .SetTerrain(false)
+                .AddTribes(AbnormalPlugin.TribeMechanical)
                 .SetDefaultEvolutionName("1.76 GHz")
                 .Build(CardHelper.CardType.Common, RiskLevel.Teth);
 
@@ -27,6 +32,7 @@ namespace WhistleWindLobotomyMod {
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
                 .AddAbilities(Ability.BuffEnemy)
                 .SetTerrain(false)
+                .AddTribes(AbnormalPlugin.TribeMechanical)
                 .SetDefaultEvolutionName("1.76 GHz")
                 .Build(CardHelper.CardType.Common, RiskLevel.Teth, true);
         }
