e574e1ce5c5d5251064f1a155b5f547871de8b1c
WhistleWind <humanhaterq@gmail.com>
2025-07-10 22:59:16 -0700

    tangle artwork

--- WhistleWindLobotomyMod/Cards/WonderLab/tangle.cs
+++ WhistleWindLobotomyMod/Cards/WonderLab/tangle.cs
@@ -9,7 +9,7 @@ namespace WhistleWindLobotomyMod {
         private static void Tangle() {
             string textureName = "tangle";
             CardManager.New(LobotomyPlugin.wonderlabPrefix, tangle, "Tangle",
-                attack: 2, health: 3)
+                attack: 2, health: 3, "A head from which sprouts endless waves of golden hair.")
                 .SetBloodCost(2)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
                 .AddAbilities(BindingStrike.ability, Driver.ability)
