423a96f0c816ff922f29df15cc7f94f518ce5e62
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2025-12-12 17:08:10 -0800

    Clarify description

--- WhistleWind.AbnormalSigils/Abilities/Ethereal.cs
+++ WhistleWind.AbnormalSigils/Abilities/Ethereal.cs
@@ -6,7 +6,7 @@ namespace WhistleWind.AbnormalSigils {
     public partial class AbnormalPlugin {
         private void Ability_Ethereal() {
             const string rulebookName = "Ethereal";
-            const string rulebookDescription = "Attacks directed at [creature] will strike the owner instead.";
+            const string rulebookDescription = "Attacks directed at [creature] will pass through and strike its owner instead.";
             Ethereal.ability = AbnormalAbilityHelper.CreateAbility<Ethereal>(
                 "sigilEthereal",
                 rulebookName, rulebookDescription, powerLevel: 0,
@@ -17,7 +17,7 @@ namespace WhistleWind.AbnormalSigils {
         }
     }
     /// <summary>
-    /// Attacks directed at [creature] will strike the owner instead.
+    /// Attacks directed at [creature] will pass through and strike its owner instead.
     /// </summary>
     public class Ethereal : AbilityBehaviour {
         public static Ability ability;
