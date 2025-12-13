8529a4c346eb49611d33bc7a69c6e54c1b08c051
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2025-12-12 20:23:59 -0800

    Adjust stats

--- WhistleWindLobotomyMod/Cards/WonderLab/titania.cs
+++ WhistleWindLobotomyMod/Cards/WonderLab/titania.cs
@@ -11,7 +11,7 @@ namespace WhistleWindLobotomyMod {
             string textureName = "titania";
             CardManager.New(LobotomyPlugin.wonderlabPrefix, titania, "Titania",
                 attack: 0, health: 4, "The queen of faeries, searching always for her traitorous husband.")
-                .SetBloodCost(1)
+                .SetBloodCost(2)
                 .SetStatIcon(FlowerPower.Icon)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
                 .AddAbilities(FlowerQueen.ability, Ability.StrafeSwap)
