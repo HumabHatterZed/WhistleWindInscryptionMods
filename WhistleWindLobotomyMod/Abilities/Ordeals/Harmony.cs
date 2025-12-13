e8c1f4f6ccaa5b016a6ad4244b848381cf291d60
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2025-12-12 23:20:59 -0800

    Fix Harmony spawning itself

--- WhistleWindLobotomyMod/Abilities/Ordeals/Harmony.cs
+++ WhistleWindLobotomyMod/Abilities/Ordeals/Harmony.cs
@@ -11,7 +11,7 @@ namespace WhistleWindLobotomyMod {
         private static void AddHarmony() {
             AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
             info.rulebookName = "Harmony";
-            info.rulebookDescription = "When [creature] dies, two Cheers for the Beginning are created on the owner's side of the board [define:wstl_skinCheers]";
+            info.rulebookDescription = "When [creature] dies, two Cheers for the Beginning are created on the owner's side of the board. [define:wstl_skinCheers]";
             info.powerLevel = 3;
             HarmonyAbility.ability = AbilityManager.Add(LobotomyPlugin.pluginGuid, info, typeof(HarmonyAbility), TextureLoader.LoadTextureFromFile("sigilHarmony.png")).Id;
         }
@@ -31,7 +31,7 @@ namespace WhistleWindLobotomyMod {
             if (openSlots.Count > 2) openSlots.RemoveRange(2, openSlots.Count - 2);
 
             foreach (CardSlot slot in openSlots) {
-                yield return BoardManager.Instance.CreateCardInSlot(CardLoader.GetCardByName(Cards.skinHarmony), slot);
+                yield return BoardManager.Instance.CreateCardInSlot(CardLoader.GetCardByName(Cards.skinCheers), slot);
             }
         }
     }
