2b43564d1943df1c215ef9156a549d867a81d78e
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2025-12-18 16:38:18 -0800

    Add ForcedOrange emission appearance

---
+++ WhistleWind.AbnormalSigils/CardAppearanceBehaviours/ForceOrange.cs
@@ -0,0 +1,17 @@
+﻿using DiskCardGame;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWind.AbnormalSigils {
+    public partial class AbnormalPlugin {
+        private void Appearance_OrangeEmission() {
+            ForcedOrangeEmission.appearance = CardHelper.CreateAppearance<ForcedOrangeEmission>(pluginGuid, "ForcedOrangeEmission").Id;
+        }
+    }
+    public class ForcedOrangeEmission : CardAppearanceBehaviour {
+        public static Appearance appearance;
+        public override void ApplyAppearance() {
+            base.Card.RenderInfo.forceEmissivePortrait = true;
+            base.Card.StatsLayer.SetEmissionColor(GameColors.Instance.orange);
+        }
+    }
+}
