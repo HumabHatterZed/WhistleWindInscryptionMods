f93faa9649e28bf9442758e5deb11ff647d6f7c1
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-09 00:40:17 -0800

    Last Helix appearance

---
+++ WhistleWindLobotomyMod/CardAppearanceBehaviours/GiantTower.cs
@@ -0,0 +1,19 @@
+﻿using DiskCardGame;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWindLobotomyMod {
+    public partial class Appearances {
+        private static void AddGiantTowerAppearance() {
+            GiantTowerAppearance.appearance = CardHelper.CreateAppearance<GiantTowerAppearance>(LobotomyPlugin.pluginGuid, "GiantTowerAppearance").Id;
+        }
+    }
+    public class GiantTowerAppearance : GiantAnimatedPortrait {
+        public static Appearance appearance;
+
+        public override void ApplyAppearance() {
+            base.ApplyAppearance();
+            base.Card.StatsLayer.SetEmissionColor(GameColors.Instance.brightLimeGreen);
+            base.Card.RenderInfo.baseTextureOverride = TextureLoader.LoadTextureFromFile("tower_cardBack.png");
+        }
+    }
+}
