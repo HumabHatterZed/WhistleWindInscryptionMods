6d8c8bed48dbc4b53ba56b795c4388a1e313e795
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-10 15:23:00 -0800

    Add support for changing num of deaths required till transformation

--- WhistleWind.AbnormalSigils/Abilities/Special Abilities/CardTransformsOnAdjacentDeath.cs
+++ WhistleWind.AbnormalSigils/Abilities/Special Abilities/CardTransformsOnAdjacentDeath.cs
@@ -8,6 +8,9 @@ namespace WhistleWind.AbnormalSigils {
     public abstract class TransformOnAdjacentDeath : SpecialCardBehaviour {
         public abstract string CardToTransformInto { get; }
         public abstract string PostEvolveDialogueId { get; }
+        public virtual int NumDeathsTillEvolve { get; } = 1;
+
+        protected int numAlliesKilled;
         public override bool RespondsToOtherCardDie(PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer) {
             // return true if the target was in an adjacent slot
             if (base.PlayableCard.OnBoard && fromCombat)
@@ -16,6 +19,11 @@ namespace WhistleWind.AbnormalSigils {
             return false;
         }
         public override IEnumerator OnOtherCardDie(PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer) {
+            numAlliesKilled++;
+            if (numAlliesKilled < NumDeathsTillEvolve) {
+                yield break;
+            }
+
             yield return new WaitForSeconds(0.15f);
             base.Card.Anim.StrongNegationEffect();
             yield return new WaitForSeconds(0.4f);
