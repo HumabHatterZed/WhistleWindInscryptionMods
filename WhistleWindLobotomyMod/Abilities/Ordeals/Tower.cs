924ccd0862aa6a25f4076dd9de7fc18b6d627b0f
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-10 14:03:54 -0800

    Adjust Tower sigil description

--- WhistleWindLobotomyMod/Abilities/Ordeals/Tower.cs
+++ WhistleWindLobotomyMod/Abilities/Ordeals/Tower.cs
@@ -14,12 +14,12 @@ namespace WhistleWindLobotomyMod {
         private static void AddTower() {
             AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
             info.rulebookName = "The Tower";
-            info.rulebookDescription = "This card enters an active state every few turns. While active, two Helix Lights will appear on the player's side of the board.";
+            info.rulebookDescription = "While this card is in an active state, the Light of the End will target spaces on the player's side of the board.";
             info.powerLevel = 5;
             info.passive = true;
 
             Tower.ability = AbilityManager.Add(LobotomyPlugin.pluginGuid, info, typeof(Tower), TextureLoader.LoadTextureFromFile("sigilTower.png")).Id;
-            info.SetUniqueRedirect("Helix Lights", "wstl:Ordeals_Helix Light", Color.green);
+            info.SetUniqueRedirect("Light of the End", "wstl:Ordeals_Light of the End", Color.green);
         }
     }
 
