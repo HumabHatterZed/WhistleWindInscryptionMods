3f7abbd1cd096653c43e09dcb8bfb800d63c448d
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-13 14:12:51 -0800

    Changed to trigger on turn end instead of upkeep

--- WhistleWind.AbnormalSigils/Abilities/Status Effects/Decay.cs
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/Decay.cs
@@ -6,25 +6,26 @@ using WhistleWind.Core.Helpers;
 
 namespace WhistleWind.AbnormalSigils {
     /// <summary>
-    /// At the start of the owner's turn, reduce this effect's Potency by 1. At 0 Potency, a card bearing this effect will kill itself.
+    /// At the end of the owner's turn, reduce this effect's Potency by 1. At 0 Potency, a card bearing this effect will kill itself.
     /// </summary>
-    public class Decay : ModifyOnUpkeepStatusEffectBehaviour {
+    public class Decay : ModifyOnTurnEndStatusEffectBehaviour {
         public static Ability iconId;
         public static SpecialTriggeredAbility specialAbility;
         public override Ability IconAbility => iconId;
         public override SpecialTriggeredAbility StatusEffect => specialAbility;
         public override int PotencyModification => -1;
-        public override IEnumerator OnModifyOnUpkeep() {
-            yield return base.OnModifyOnUpkeep();
+        public override IEnumerator OnModifyOnTurnEnd() {
+            yield return base.OnModifyOnTurnEnd();
             if (EffectPotency < 1) {
                 yield return base.PlayableCard.Die(false, base.PlayableCard);
             }
         }
+        public override int Priority => -9009;
     }
     public partial class AbnormalPlugin {
         private void StatusEffect_Decay() {
             const string rName = "Decay";
-            const string rDesc = "At the start of the owner's turn, reduce this effect's Potency by 1. At 0 Potency, a card bearing this effect will kill itself.";
+            const string rDesc = "At the end of the owner's turn, reduce this effect's Potency by 1. At 0 Potency, a card bearing this effect will kill itself.";
             StatusEffectManager.FullStatusEffect data = StatusEffectManager.New<Decay>(
                 pluginGuid, rName, rDesc, -3, GameColors.Instance.nearWhite,
                 TextureLoader.LoadTextureFromFile("sigilDecay.png", Assembly),
