b4fc711e26c009b1f6e6c18f7e889745b9870348
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-18 15:16:59 -0800

    Built out encounter

--- WhistleWindLobotomyMod/Opponents/Ordeals/Violet/OrdealVioletMidnight.cs
+++ WhistleWindLobotomyMod/Opponents/Ordeals/Violet/OrdealVioletMidnight.cs
@@ -1,9 +1,37 @@
 ﻿using DiskCardGame;
+using InscryptionAPI.Encounters;
+using System.Collections.Generic;
 
 namespace WhistleWindLobotomyMod.Opponents {
     public class OrdealVioletMidnight : OrdealBattleSequencer {
+        public override void TryAddOrdealRandomBuff(PlayableCard card) {
+            if (card.HasAbility(Delusion.ability)) {
+                card.Info.Mods.Add(new(0, 3 * RunState.CurrentRegionTier + 2 * (RunState.Run.DifficultyModifier - 1)));
+            }
+        }
         public override int ConstructOrdealBlueprint(EncounterData encounterData, int baseDifficulty) {
-            return 0;
+            EncounterData.StartCondition cond = new();
+            List<CardInfo> midnightOrdeals = new() {
+                CardLoader.GetCardByName(Cards.godDelusionR),
+                CardLoader.GetCardByName(Cards.godDelusionW),
+                CardLoader.GetCardByName(Cards.godDelusionB),
+                CardLoader.GetCardByName(Cards.godDelusionP)
+            };
+            midnightOrdeals = new(midnightOrdeals.Randomize());
+            if (RunState.CurrentRegionTier + RunState.Run.DifficultyModifier > 1) {
+                cond.cardsInOpponentSlots = midnightOrdeals.ToArray();
+            }
+            else {
+                cond.cardsInOpponentQueue = midnightOrdeals.ToArray();
+            }
+            encounterData.startConditions.Add(cond);
+
+            HighestPositiveScaleBalance = 0;
+            ValidCards.Add(Cards.godDelusionR);
+            ValidCards.Add(Cards.godDelusionW);
+            ValidCards.Add(Cards.godDelusionB);
+            ValidCards.Add(Cards.godDelusionP);
+            return 4;
         }
     }
 }
