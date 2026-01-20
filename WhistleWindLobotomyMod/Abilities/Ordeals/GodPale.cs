4bdd1549875f55d1be61db9a9e0e4a8b76136aca
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-19 21:11:38 -0800

    Fix Pale Eye animations

--- WhistleWindLobotomyMod/Abilities/Ordeals/GodPale.cs
+++ WhistleWindLobotomyMod/Abilities/Ordeals/GodPale.cs
@@ -32,7 +32,7 @@ namespace WhistleWindLobotomyMod {
 
         private CardSlot eyeSlot = null;
         private Animator eyeAnim;
-        private bool active = false;
+        private bool active = true;
         private bool preActivation = false;
         private CardSlot DetermineNewSlot() {
             List<CardSlot> slots = BoardManager.Instance.PlayerSlotsCopy;
@@ -43,19 +43,23 @@ namespace WhistleWindLobotomyMod {
         }
 
         private IEnumerator MoveEyeToSlot() {
-            Tween.Position(activateVisualGameObject.transform, eyeSlot.transform.position + new Vector3(0, 0.2f, 0), 1f, 0.5f, startCallback: () => ShowEye(true));
-            yield return new WaitForSeconds(0.5f);
-            AudioController.Instance.PlaySound2D("Violet_pale_move", MixerGroup.TableObjectsSFX);
-            yield return new WaitForSeconds(1.5f);
+            LobotomyPlugin.Log.LogDebug("[PaleEye] Move to slot");
+            ShowEye(true);
+            AudioController.Instance.PlaySound2D("Violet_eye_move", MixerGroup.TableObjectsSFX);
+            yield return new WaitForSeconds(0.2f);
+            Tween.Position(activateVisualGameObject.transform, eyeSlot.transform.position + new Vector3(0, 0.2f, 0), 0.5f, 0f);
+            yield return new WaitForSeconds(0.75f);
         }
 
         private void ShowEye(bool show) {
-            AudioController.Instance.PlaySound2D("Violet_pale_start", MixerGroup.TableObjectsSFX);
+            AudioController.Instance.PlaySound2D("Violet_eye_start", MixerGroup.TableObjectsSFX);
             if (show) {
-                eyeAnim.SetTrigger("show");
+                LobotomyPlugin.Log.LogDebug("[PaleEye] Toggle Show");
+                eyeAnim.Play("eye_show");
             }
             else {
-                eyeAnim.SetTrigger("hide");
+                LobotomyPlugin.Log.LogDebug("[PaleEye] Toggle hide");
+                eyeAnim.Play("eye_hide");
             }
             active = show;
         }
@@ -78,7 +82,7 @@ namespace WhistleWindLobotomyMod {
             }
                 
             yield return MoveEyeToSlot(); // move eye to new slot
-            preActivation = true;
+            preActivation = false;
         }
 
         public override IEnumerator OnResolveOnBoard() {
@@ -86,26 +90,37 @@ namespace WhistleWindLobotomyMod {
             eyeSlot = base.Card.OpposingSlot();
             activateVisualGameObject.transform.position = eyeSlot.transform.position + new Vector3(0, 0.2f, 0);
             ViewManager.Instance.SwitchToView(View.Board);
+            AudioController.Instance.PlaySound2D("Violet_eye_start", MixerGroup.TableObjectsSFX);
             activateVisualGameObject.transform.GetChild(0).gameObject.SetActive(true);
-            ShowEye(true);
+            active = true;
             yield return new WaitForSeconds(0.5f);
         }
 
         public override void SetUpVisualGameObject() {
-            activateVisualGameObject = GameObject.Instantiate(LobOpponentUtils.ShrineBossPalePrefab);
-            eyeAnim = activateVisualGameObject.GetComponent<Animator>();
+            if (activateVisualGameObject == null) {
+                activateVisualGameObject = GameObject.Instantiate(LobOpponentUtils.ShrineBossPalePrefab);
+                eyeAnim = activateVisualGameObject.GetComponent<Animator>();
+            }
         }
 
         public override bool RespondsToTurnEnd(bool playerTurnEnd) => !preActivation;
         public override IEnumerator OnTurnEnd(bool playerTurnEnd) {
-            int numShrines = 1 + BoardManager.Instance.GetOpponentCards(x => x.HasTrait(LobotomyCardManager.Ordeal)).Count;
-            if (Random.Range(0, 7 + RunState.CurrentRegionTier + RunState.Run.DifficultyModifier  - numShrines) == 0) {
-                ShowEye(!active);
-                yield return new WaitForSeconds(1f);
+            if (active) {
+                // higher chance of eye disappearing when other shrines are active
+                int numShrines = BoardManager.Instance.GetOpponentCards(x => x.HasTrait(LobotomyCardManager.Ordeal)).Count;
+                if (numShrines > 1 && Random.Range(0, 7 + RunState.CurrentRegionTier + RunState.Run.DifficultyModifier - numShrines) == 0) {
+                    ShowEye(false);
+                    yield return new WaitForSeconds(0.5f);
+                }
+            }
+            else if (RunState.CurrentRegionTier > 2 || RunState.Run.DifficultyModifier > 3 || Random.Range(0, 4 - RunState.Run.DifficultyModifier) == 0) {
+                ShowEye(true);
+                yield return new WaitForSeconds(0.5f);
             }
 
+
             if (active && eyeSlot.Card != null) {
-                AudioController.Instance.PlaySound2D("Violet_pale_start", MixerGroup.TableObjectsSFX);
+                AudioController.Instance.PlaySound2D("Violet_eye_start", MixerGroup.TableObjectsSFX);
                 eyeSlot.Card.Status.damageTaken += Mathf.CeilToInt(eyeSlot.Card.Health / 4f);
                 if (eyeSlot.Card.Health <= 0) {
                     yield return eyeSlot.Card.Die(false, null, false);
