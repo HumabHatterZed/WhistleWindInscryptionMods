9f96c924187240b5157076e745e2a3e5e49b1939
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-22 23:44:33 -0800

    Simplify initialisation

--- Core/Helpers/CombatHelpers.cs
+++ Core/Helpers/CombatHelpers.cs
@@ -42,10 +42,11 @@ namespace Core.Helpers {
             int randomSeed = SaveManager.SaveFile.GetCurrentRandomSeed();
             List<CardSlot> openSlots = BoardManager.Instance.OpponentSlotsCopy.Where(x => !TurnManager.Instance.Opponent.QueuedSlots.Contains(x)).ToList();
             openSlots.RemoveAll(x => x.Card != null && x.Card.HasTrait(Trait.Giant));
+            // if we cannot add to the queue, add to the turn plan instead
             if (openSlots.Count == 0) {
-                List<List<CardInfo>> turnPlan = Singleton<TurnManager>.Instance.Opponent.TurnPlan;
-                List<CardInfo> addInfo = new() { cardToQueue };
-                turnPlan.Add(addInfo);
+                List<List<CardInfo>> turnPlan = new(Singleton<TurnManager>.Instance.Opponent.TurnPlan) {
+                    new() { cardToQueue }
+                };
                 yield return Singleton<TurnManager>.Instance.Opponent.ModifyTurnPlan(turnPlan);
             }
             else {
