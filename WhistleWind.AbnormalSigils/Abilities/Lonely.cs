72b0713b311e1b8017f3bfaa21ec040da174b457
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-23 17:46:39 -0800

    Overhaul Lonely and Pebble

--- WhistleWind.AbnormalSigils/Abilities/Lonely.cs
+++ WhistleWind.AbnormalSigils/Abilities/Lonely.cs
@@ -1,25 +1,29 @@
-﻿using DiskCardGame;
+﻿using Core.Helpers;
+using DiskCardGame;
 using HarmonyLib;
+using InscryptionAPI.Card;
 using InscryptionAPI.Helpers.Extensions;
 using InscryptionAPI.RuleBook;
+using InscryptionAPI.Triggers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using WhistleWind.AbnormalSigils.Core.Helpers;
 using WhistleWind.AbnormalSigils.StatusEffects;
+using WhistleWind.Core.AbilityClasses;
 using WhistleWind.Core.Helpers;
 
 namespace WhistleWind.AbnormalSigils {
     public partial class AbnormalPlugin {
         private void Ability_Lonely() {
-            const string rulebookName = "Pebble Giver";
-            const string rulebookDescription = "Choose one of your cards to gain Pebble unless a card with Pebble already exists, then return this card to your hand.";
+            const string rulebookName = "Lonely";
+            const string rulebookDescription = "Give Pebble to a chosen creature, then return this card to your hand. When a card with Pebble perishes, kill all ally cards with Pebble and inflict Grief on the remaining allies.";
             const string dialogue = "A friend to stay.";
             Lonely.ability = AbnormalAbilityHelper.CreateAbility<Lonely>(
                 "sigilLonely",
-                rulebookName, rulebookDescription, dialogue, powerLevel: 2,
-                modular: false, opponent: false, canStack: false)
+                rulebookName, rulebookDescription, dialogue, powerLevel: 2)
                 .SetAbilityRedirect("Pebble", Pebble.iconId, GameColors.Instance.gray)
+                .SetAbilityRedirect("Grief", Grief.iconId, GameColors.Instance.red)
                 .Id;
         }
     }
@@ -27,52 +31,52 @@ namespace WhistleWind.AbnormalSigils {
     /// Choose one of your cards to gain Pebble unless a card with Pebble already exists, then return this card to your hand.
     /// </summary>
     [HarmonyPatch]
-    public class Lonely : AbilityBehaviour {
+    public class Lonely : AbilityBehaviour, IOnOtherCardDieInHand {
         public static Ability ability;
         public override Ability Ability => ability;
-
-        public override bool RespondsToSlotTargetedForAttack(CardSlot slot, PlayableCard attacker) => base.Card == attacker && CheckValid(slot);
+        public bool IsValidTarget(CardSlot slot) {
+            if (slot.Card != null && !slot.Card.HasStatusEffect<Pebble>() && slot.Card.LacksAllTraits(Trait.Terrain, Trait.Pelt)) {
+                return base.Card.OpponentCard == slot.Card.OpponentCard || (slot.Card.OpponentCard && base.Card.OriginatedFromQueue);
+            }
+            return false;
+        }
+        public override bool RespondsToSlotTargetedForAttack(CardSlot slot, PlayableCard attacker) => IsValidTarget(slot);
         public override IEnumerator OnSlotTargetedForAttack(CardSlot slot, PlayableCard attacker) {
             yield return slot.Card.AddStatusEffectToFaceDown<Pebble>();
-            if (base.Card.OpponentCard) {
-                List<CardSlot> slots = BoardManager.Instance.OpponentSlotsCopy.FindAll(x => !TurnManager.Instance.Opponent.QueuedSlots.Contains(x));
-                if (slots.Count == 0)
-                    yield break;
-
-                yield return HelperMethods.ChangeCurrentView(View.OpponentQueue);
-                yield return TurnManager.Instance.Opponent.QueueCard(base.Card.Info.Clone() as CardInfo, slots[SeededRandom.Range(0, slots.Count, base.GetRandomSeed())]);
-                yield return new WaitForSeconds(0.4f);
-            }
-            else {
-                yield return HelperMethods.ChangeCurrentView(View.Default);
-                yield return CardSpawner.Instance.SpawnCardToHand(base.Card.Info.Clone() as CardInfo);
-                yield return new WaitForSeconds(0.4f);
-            }
-
-            yield return HelperMethods.ChangeCurrentView(View.Board);
+            yield return CombatHelpers.QueueOrCreateDrawnCard(base.Card.Info.Clone() as CardInfo, slot.Card.OpponentCard);
             yield return base.LearnAbility(0.4f);
         }
 
-        public bool HasFriend {
-            get {
-                bool isOpponent = base.Card.OpponentCard || base.Card.QueuedSlot != null;
-                if (BoardManager.Instance.GetCards(!isOpponent).Exists(x => x.HasStatusEffect<Pebble>()))
-                    return true;
+        public bool RespondsToOtherCardDieInHand(PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer) {
+            return card.OpponentCard == base.Card.OpponentCard && card.HasStatusEffect<Pebble>();
+        }
 
-                if (isOpponent)
-                    return TurnManager.Instance.Opponent.Queue.Exists(x => x.HasStatusEffect<Pebble>());
-                else
-                    return PlayerHand.Instance.CardsInHand.Exists(x => x.HasStatusEffect<Pebble>());
-
-
+        public IEnumerator OnOtherCardDieInHand(PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer) {
+            List<PlayableCard> cards = BoardManager.Instance.GetCards(!base.Card.OpponentCard, x => !x.Dead);
+            foreach (PlayableCard c in cards) {
+                if (c.HasStatusEffect<Pebble>()) {
+                    yield return c.Die(false);
+                }
+                else {
+                    yield return c.AddStatusEffect<Grief>(1, modifyTurnGained: delegate (int i) { return ModifyTurnGriefGained(c, i); });
+                }
             }
         }
-        private bool CheckValid(CardSlot target) => target.IsOpponentSlot() == base.Card.OpponentCard && target.Card != null && !HasFriend;
 
-        [HarmonyPostfix, HarmonyPatch(typeof(Opponent), nameof(Opponent.QueuedCardIsBlocked))]
-        private static void DontPlayLonelyIfHasFriend(ref bool __result, PlayableCard queuedCard) {
-            if (queuedCard != null && queuedCard.HasAbility(ability) && queuedCard.GetComponent<Lonely>().HasFriend)
-                __result = true;
+        private int ModifyTurnGriefGained(PlayableCard c, int i) {
+            // if the card was killed during the opponent's turn, don't modify the turn number
+            if (c.OpponentCard != TurnManager.Instance.IsPlayerTurn) {
+                return i;
+            }
+            return i + 1;
+        }
+
+        [HarmonyPatch(typeof(Opponent), nameof(Opponent.QueuedCardIsBlocked))]
+        private static void DontPlayLonelyIfAllPebbles(PlayableCard queuedCard, ref bool __result) {
+            if (!__result && queuedCard.HasAbility(Lonely.ability)) {
+                // don't play Lonely cards from the queue if the opponent doesn't have any valid target cards
+                __result = BoardManager.Instance.GetOpponentCards(x => x.LacksAllTraits(Trait.Terrain, Trait.Pelt) && !x.HasStatusEffect<Pebble>()).Count == 0;
+            }
         }
     }
 }
--- WhistleWind.AbnormalSigils/Abilities/Status Effects/Pebble.cs
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/Pebble.cs
@@ -1,51 +1,90 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Helpers.Extensions;
+using InscryptionAPI.RuleBook;
+using InscryptionAPI.Triggers;
 using System.Collections;
 using UnityEngine;
 using WhistleWind.AbnormalSigils.StatusEffects;
 using WhistleWind.Core.Helpers;
 
 namespace WhistleWind.AbnormalSigils {
-    public class Pebble : StatusEffectBehaviour {
+    /// <summary>
+    /// At the end of the owner's turn, this card regains 1 Health. While there are other allies with this effect, this card gains 1 Power.
+    /// </summary>
+    public class Pebble : StatusEffectBehaviour, IPassiveAttackBuff {
         public static Ability iconId;
         public static SpecialTriggeredAbility specialAbility;
         public override Ability IconAbility => iconId;
         public override SpecialTriggeredAbility StatusEffect => specialAbility;
+        
+        internal static StatusEffectManager.FullStatusEffect data;
+
+        private bool lonelyCardExists = true;
 
         public override bool RespondsToTurnEnd(bool playerTurnEnd) => base.PlayableCard.OpponentCard != playerTurnEnd;
         public override IEnumerator OnTurnEnd(bool playerTurnEnd) {
+            RecalculateLoneliness();
+
             if (base.PlayableCard.Health < base.PlayableCard.MaxHealth) {
                 base.PlayableCard.Anim.LightNegationEffect();
-                base.PlayableCard.HealDamage(1);
-                yield return new WaitForSeconds(0.2f);
+                yield return base.PlayableCard.Heal(1, 0.2f);
             }
         }
-        public override bool RespondsToUpkeep(bool playerUpkeep) => base.PlayableCard.OpponentCard != playerUpkeep;
-        public override IEnumerator OnUpkeep(bool playerUpkeep) => OnTurnEnd(playerUpkeep);
-
-        public override bool RespondsToDie(bool wasSacrifice, PlayableCard killer) => true;
 
         public override IEnumerator OnDie(bool wasSacrifice, PlayableCard killer) {
-            foreach (PlayableCard card in BoardManager.Instance.GetCards(!base.PlayableCard.OpponentCard)) {
-                yield return card.AddStatusEffectToFaceDown<Grief>(1, modifyTurnGained: delegate (int x) {
-                    return x + 1;
-                });
-            }
-
+            RecalculateLoneliness();
             yield return DialogueHelper.PlayDialogueEvent("LonelyDie", card: base.PlayableCard);
         }
-        internal static StatusEffectManager.FullStatusEffect data;
+
+        public override bool RespondsToOtherCardDie(PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer) => true;
+        public override IEnumerator OnOtherCardDie(PlayableCard card, CardSlot deathSlot, bool fromCombat, PlayableCard killer) {
+            RecalculateLoneliness();
+            yield break;
+        }
+
+        public override bool RespondsToOtherCardAssignedToSlot(PlayableCard otherCard) => true;
+        public override IEnumerator OnOtherCardAssignedToSlot(PlayableCard otherCard) {
+            RecalculateLoneliness();
+            yield break;
+        }
+
+        public override bool RespondsToStatusEffectAdded(PlayableCard target, int amount, StatusEffectBehaviour statusEffect, bool alreadyHasStatus) {
+            return target.OpponentCard == base.PlayableCard.OpponentCard && statusEffect.IconAbility == this.IconAbility;
+        }
+        public override IEnumerator OnStatusEffectAdded(PlayableCard target, int amount, StatusEffectBehaviour statusEffect, bool alreadyHasStatus) {
+            RecalculateLoneliness();
+            yield break;
+        }
+        public override bool RespondsToStatusEffectRemoved(PlayableCard target, StatusEffectBehaviour statusEffect) {
+            return target.OpponentCard == base.PlayableCard.OpponentCard && statusEffect.IconAbility == this.IconAbility;
+        }
+        public override IEnumerator OnStatusEffectRemoved(PlayableCard target, StatusEffectBehaviour statusEffect) {
+            RecalculateLoneliness();
+            yield break;
+        }
+        private void RecalculateLoneliness() {
+            lonelyCardExists = BoardManager.Instance.GetCards(!base.PlayableCard.OpponentCard, x => x.HasStatusEffect<Pebble>()).Count > 1;
+        }
+
+        public int GetPassiveAttackBuff(PlayableCard target) {
+            if (target == base.PlayableCard && lonelyCardExists) {
+                return 1;
+            }
+            return 0;
+        }
     }
+
     public partial class AbnormalPlugin {
         private void StatusEffect_Pebble() {
             const string rName = "Pebble";
-            const string rDesc = "At the start and end of the owner's turn, a card bearing this effect regains 1 Health. When this card perishes, inflict Grief on all allied creatures.";
+            const string rDesc = "At the end of the owner's turn, this card regains 1 Health. While there are other allies with this effect, this card gains 1 Power.";
             Pebble.data = StatusEffectManager.New<Pebble>(
-                pluginGuid, rName, rDesc, 2, GameColors.Instance.nearWhite,
+                pluginGuid, rName, rDesc, 3, GameColors.Instance.nearWhite,
                 TextureLoader.LoadTextureFromFile("sigilPebble.png", Assembly),
                 TextureLoader.LoadTextureFromFile("sigilPebble_pixel.png", Assembly))
                 .AddMetaCategories(StatusMetaCategory.Part1StatusEffect, StatusMetaCategory.Part3StatusEffect, StatusMetaCategory.GrimoraStatusEffect, StatusMetaCategory.MagnificusStatusEffect);
 
+            Pebble.data.IconInfo.SetAbilityRedirect("Lonely", Lonely.ability, GameColors.Instance.limeGreen);
             Pebble.specialAbility = Pebble.data.Id;
             Pebble.iconId = Pebble.data.IconInfo.ability;
         }
