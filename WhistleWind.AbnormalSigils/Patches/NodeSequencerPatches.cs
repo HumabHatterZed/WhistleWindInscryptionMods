a81f8bd62bf27ab11cc95543b8c0e428c3ed328f
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-23 22:21:17 -0800

    Prevent Sniper and Activated Sniper sigils from being merged onto the same card

--- WhistleWind.AbnormalSigils/Patches/NodeSequencerPatches.cs
+++ WhistleWind.AbnormalSigils/Patches/NodeSequencerPatches.cs
@@ -8,14 +8,30 @@ namespace WhistleWind.AbnormalSigils {
     internal class CardMergePatches {
         // Prevents cards from being sacrificed / transferring their sigils
         [HarmonyPostfix, HarmonyPatch(nameof(CardMergeSequencer.GetValidCardsForSacrifice))]
-        private static void RemoveFromValidCardsForSacrifice(ref List<CardInfo> __result) {
+        private static void RemoveFromValidCardsForSacrifice(CardInfo host, ref List<CardInfo> __result) {
             __result.RemoveAll(x => x.HasCardMetaCategory(AbnormalPlugin.CannotGiveSigils));
+            if (host != null) {
+                if (host.HasAbility(Ability.Sniper)) {
+                    __result.RemoveAll(x => x.HasAbility(ActivatedSniper.ability));
+                }
+                if (host.HasAbility(ActivatedSniper.ability)) {
+                    __result.RemoveAll(x => x.HasAbility(Ability.Sniper));
+                }
+            }
         }
 
         // Prevents card from being merged / gaining sigils
         [HarmonyPostfix, HarmonyPatch(nameof(CardMergeSequencer.GetValidCardsForHost))]
-        private static void RemoveFromValidCardsForHost(ref List<CardInfo> __result) {
+        private static void RemoveFromValidCardsForHost(CardInfo sacrifice, ref List<CardInfo> __result) {
             __result.RemoveAll(x => x.HasCardMetaCategory(AbnormalPlugin.CannotGainSigils));
+            if (sacrifice != null) {
+                if (sacrifice.HasAbility(Ability.Sniper)) {
+                    __result.RemoveAll(x => x.HasAbility(ActivatedSniper.ability));
+                }
+                if (sacrifice.HasAbility(ActivatedSniper.ability)) {
+                    __result.RemoveAll(x => x.HasAbility(Ability.Sniper));
+                }
+            }
         }
     }
 
