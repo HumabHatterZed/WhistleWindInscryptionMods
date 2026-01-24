736de385c51afc496651c1d8466ddbf86f6b39f7
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-23 21:34:13 -0800

    Allow Ethereal cards to be hammered, rewrite description

--- WhistleWind.AbnormalSigils/Abilities/Ethereal.cs
+++ WhistleWind.AbnormalSigils/Abilities/Ethereal.cs
@@ -1,5 +1,6 @@
 ﻿using DiskCardGame;
 using HarmonyLib;
+using InscryptionAPI.Card;
 using System.Collections;
 using WhistleWind.AbnormalSigils.Core.Helpers;
 
@@ -7,7 +8,7 @@ namespace WhistleWind.AbnormalSigils {
     public partial class AbnormalPlugin {
         private void Ability_Ethereal() {
             const string rulebookName = "Ethereal";
-            const string rulebookDescription = "[creature] cannot take damage from or deal damage to opposing creatures. Instead, attacks from and against this card will always strike directly.";
+            const string rulebookDescription = "[creature] cannot be damaged indirectly, and attacks from or towards this card will always strike directly instead.";
             Ethereal.ability = AbnormalAbilityHelper.CreateAbility<Ethereal>(
                 "sigilEthereal",
                 rulebookName, rulebookDescription, powerLevel: 0,
@@ -16,7 +17,7 @@ namespace WhistleWind.AbnormalSigils {
         }
     }
     /// <summary>
-    /// [creature] cannot take from or deal damage to opposing creatures. Instead, attacks from and against this card will always strike directly.
+    /// [creature] cannot be damaged indirectly, and attacks from or towards this card will always strike directly instead.
     /// </summary>
     [HarmonyPatch]
     public class Ethereal : AbilityBehaviour {
@@ -24,8 +25,12 @@ namespace WhistleWind.AbnormalSigils {
         public override Ability Ability => ability;
 
         [HarmonyPostfix, HarmonyPatch(typeof(PlayableCard), nameof(PlayableCard.TakeDamage))]
-        private static IEnumerator PreventDamageFromIndirectSources(IEnumerator result, PlayableCard __instance, PlayableCard attacker) {
-            if (__instance.HasAbility(ability) || (attacker != null && attacker.HasAbility(ability))) {
+        private static IEnumerator PreventDamageFromIndirectSources(IEnumerator result, PlayableCard __instance, int damage, PlayableCard attacker) {
+            // allow for hammer usage (hammer deals 100 damage flat, but we want to account for damage reduction
+            if (__instance.HasAbility(ability) && (attacker != null || damage < 90)) {
+                yield break;
+            }
+            if (attacker != null && attacker.HasAbility(ability)) {
                 yield break;
             }
             yield return result;
