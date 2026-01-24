2ae620cd5c8b24e52c0f080422ee57015842ad13
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-24 15:34:17 -0800

    Fix modularity being tied to rulebook

--- WhistleWind.AbnormalSigils/AbnormalPlugin.cs
+++ WhistleWind.AbnormalSigils/AbnormalPlugin.cs
@@ -320,7 +320,7 @@ namespace WhistleWind.AbnormalSigils {
                 }
 
                 bool rulebook = ability.Info.GetExtendedPropertyAsBool(AbnormalAbilityHelper.ADDTORULEBOOK) == true;
-                bool modular = ability.Info.GetExtendedPropertyAsBool(AbnormalAbilityHelper.ADDTORULEBOOK) == true;
+                bool modular = ability.Info.GetExtendedPropertyAsBool(AbnormalAbilityHelper.FORCEMODULAR) == true;
 
                 // abilities marked modular are always modular in Act 1
                 if (validCards.Exists(x => x.temple == CardTemple.Nature) || modular) {
