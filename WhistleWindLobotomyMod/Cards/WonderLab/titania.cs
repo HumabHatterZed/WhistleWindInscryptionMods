db7bb34bbfe368548185f1ef0c8212a1f7da990e
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-24 17:04:07 -0800

    Remove Flower Power stat icon, rebalance Titania

--- WhistleWindLobotomyMod/Cards/WonderLab/titania.cs
+++ WhistleWindLobotomyMod/Cards/WonderLab/titania.cs
@@ -10,9 +10,9 @@ namespace WhistleWindLobotomyMod {
         private static void Titania() {
             string textureName = "titania";
             CardManager.New(LobotomyPlugin.wonderlabPrefix, titania, "Titania",
-                attack: 0, health: 4, "The queen of faeries, searching always for her traitorous husband.")
+                attack: 1, health: 5, "The queen of faeries, searching always for her traitorous husband.")
                 .SetBloodCost(2)
-                .SetStatIcon(FlowerPower.Icon)
+                //.SetStatIcon(FlowerPower.Icon)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName)
                 .AddAbilities(FlowerQueen.ability, Ability.StrafeSwap)
                 .AddTribes(AbnormalPlugin.TribeFae)
