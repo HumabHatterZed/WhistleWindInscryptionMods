0cf691cae5c7c46d2258ab7aecb36255157d4e30
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-01-24 17:40:25 -0800

    Spells no longer meet the requirements for 'can be played by turn 2'

--- WhistleWindLobotomyMod/Patches/UniqueInteractionPatches.cs
+++ WhistleWindLobotomyMod/Patches/UniqueInteractionPatches.cs
@@ -1,9 +1,19 @@
 ﻿using DiskCardGame;
 using HarmonyLib;
+using Infiniscryption.Spells.Patchers;
 
 namespace WhistleWindLobotomyMod.Patches {
     [HarmonyPatch]
     internal class UniqueInteractionPatches {
+        [HarmonyPrefix, HarmonyPatch(typeof(Deck), nameof(Deck.CardCanBePlayedByTurn2WithHand))]
+        private static bool SpellsCannotBePlayedByTurn2(CardInfo card, ref bool __result) {
+            if (card.IsSpell()) {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+
         [HarmonyPrefix, HarmonyPatch(typeof(DrawRabbits), nameof(DrawRabbits.CardToDraw), MethodType.Getter)]
         private static bool InfiniteTrainingDummies(DrawRabbits __instance, ref CardInfo __result) {
             if (__instance.Card.Info.name == Cards.trainingDummy) {
