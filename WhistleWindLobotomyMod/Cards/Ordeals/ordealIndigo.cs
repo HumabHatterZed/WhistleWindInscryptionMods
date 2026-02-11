57963e140a6cf1fabd33fe52c19b90120053de18
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-02-11 10:54:47 -0800

    Sweeper Corpse sprites

--- WhistleWindLobotomyMod/Cards/Ordeals/ordealIndigo.cs
+++ WhistleWindLobotomyMod/Cards/Ordeals/ordealIndigo.cs
@@ -23,6 +23,7 @@ namespace WhistleWindLobotomyMod {
                 attack: 0, health: 1)
                 .SetPortraits(LobotomyPlugin.ModAssembly, "sweeperCorpse")
                 .AddAbilities(StartingDecay.ability)
+                .AddAppearances(CardAppearanceBehaviour.Appearance.RedEmission)
                 .SetTerrain()
                 .SetOrdealCard(OrdealType.Indigo)
                 .RemoveTraits(Ordeal, Trait.Terrain)
