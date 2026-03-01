3817d8266f320251d3ae0bcd21c54fd25a12ecae
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-02-28 18:32:34 -0800

    Rebalance stats, give Dusk and Midnight cards Ants stat icon

--- WhistleWindLobotomyMod/Cards/Ordeals/ordealAmber.cs
+++ WhistleWindLobotomyMod/Cards/Ordeals/ordealAmber.cs
@@ -1,6 +1,7 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Card;
 using WhistleWind.AbnormalSigils;
+using WhistleWind.AbnormalSigils.Core.Helpers;
 using WhistleWind.Core.Helpers;
 using WhistleWindLobotomyMod.Opponents;
 using static WhistleWind.AbnormalSigils.AbnormalPlugin;
@@ -22,28 +23,32 @@ namespace WhistleWindLobotomyMod {
                 .AddAbilities()
                 .AddAppearances(ForcedOrangeEmission.appearance)
                 .AddTribes(Tribe.Insect)
+                .AddTraits(Trait.Ant)
                 .SetOrdealCard(Opponents.OrdealType.Amber)
                 .Build();
 
-            CardInfo chain = CardManager.New(LobotomyPlugin.pluginPrefix, foodChain, OrdealUtils.GetOrdealTitle(OrdealType.Amber, 2),
-                attack: 2, health: 5)
+            CardManager.New(LobotomyPlugin.pluginPrefix, foodChain, OrdealUtils.GetOrdealTitle(OrdealType.Amber, 2),
+                attack: 0, health: 4)
                 .SetBonesCost(18)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName2)
                 .AddAbilities(Challenging.ability, Food.ability)
                 .AddAppearances(ForcedOrangeEmission.appearance)
                 .AddTribes(Tribe.Insect)
-                .AddTraits(PriorityMovement)
+                .SetStatIcon(SpecialStatIcon.Ants)
+                .AddTraits(PriorityMovement, Trait.Ant)
                 .SetOrdealCard(Opponents.OrdealType.Amber)
                 .Build();
 
-            CardInfo meal = CardManager.New(LobotomyPlugin.pluginPrefix, eternalMeal, OrdealUtils.GetOrdealTitle(OrdealType.Amber, 3),
-                attack: 2, health: 20)
+            CardManager.New(LobotomyPlugin.pluginPrefix, eternalMeal, OrdealUtils.GetOrdealTitle(OrdealType.Amber, 3),
+                attack: 0, health: 8)
                 .SetBonesCost(40)
                 .SetPortraits(LobotomyPlugin.ModAssembly, textureName3)
-                .AddAbilities(Challenging.ability, Survival.ability)
+                .AddAbilities(Ability.AllStrike, Ability.Reach, Challenging.ability, Survival.ability)
                 .AddAppearances(ForcedOrangeEmission.appearance)
                 .AddTribes(Tribe.Insect)
-                .AddTraits(Trait.Uncuttable, ImmuneToInstaDeath, PriorityMovement)
+                .SetStatIcon(SpecialStatIcon.Ants)
+                .AddTraits(Trait.Uncuttable, ImmuneToInstaDeath, PriorityMovement, Trait.Ant)
+                .SetMiniGiant()
                 .SetOrdealCard(Opponents.OrdealType.Amber)
                 .Build();
         }
