4646d39f253559271e428dbd9560cad315c2c2f9
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-03-04 13:47:33 -0800

    Rebalance cost

--- WhistleWind.AbnormalSigils/Abilities/NeuteredLatch.cs
+++ WhistleWind.AbnormalSigils/Abilities/NeuteredLatch.cs
@@ -9,25 +9,24 @@ namespace WhistleWind.AbnormalSigils {
     public partial class AbnormalPlugin {
         private void Ability_NeuteredLatch() {
             const string rulebookName = "Neutered Latch";
-            const string rulebookDescription = "Once per turn, pay [sigilcost:2 Bones] to choose a creature to gain the Neutered sigil, then increase this sigil's activation cost by 1 Bone.";
+            const string rulebookDescription = "Pay 3 Energy to choose a creature to gain the Neutered sigil.";
             const string dialogue = "The will to fight has been lost.";
             const string triggerText = "[creature] prevents the chosen creature from attacking.";
             NeuteredLatch.ability = AbnormalAbilityHelper.CreateActivatedAbility<NeuteredLatch>(
                 "sigilNeuteredLatch",
-                rulebookName, rulebookDescription, dialogue, triggerText, powerLevel: 4)
+                rulebookName, rulebookDescription, dialogue, triggerText, powerLevel: 3)
                 .SetAbilityRedirect("Neutered", Neutered.ability, GameColors.Instance.gray)
                 .Id;
         }
     }
     /// <summary>
-    /// Once per turn, pay [sigilcost:2 Bones] to choose a creature to gain the Neutered sigil, then increase this sigil's activation cost by 1 Bone.
+    /// Pay 3 Energy to choose a creature to gain the Neutered sigil.
     /// </summary>
     public class NeuteredLatch : ActivatedSelectSlotBehaviour {
         public static Ability ability;
         public override Ability Ability => ability;
         public override Ability LatchAbility => Neutered.ability;
-        public override int StartingBonesCost => 2;
-        public override int OnActivateBonesCostMod => 1;
+        public override int StartingEnergyCost => 3;
 
         public override bool IsValidTarget(CardSlot slot) {
             if (!base.IsValidTarget(slot))
