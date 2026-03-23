f158fd2ee8b874bc7a93c34d56e9b424aa5de0c7
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-03-23 12:52:27 -0700

    Add Staining Rose challenge stub

---
+++ WhistleWindLobotomyMod/Challenges/StartingRose.cs
@@ -0,0 +1,24 @@
+﻿using DiskCardGame;
+using InscryptionAPI.Ascension;
+using System.Linq;
+using WhistleWind.Core.Helpers;
+
+namespace WhistleWindLobotomyMod.Challenges {
+    public static class StartingRose {
+        private const string TITLE = "Staining Rose";
+        private const string DESCRIPTION = "The Adult boss will be stronger and guaranteed to appear.";
+
+        public static AscensionChallenge ID { get; private set; }
+
+        internal static void Register() {
+            ID = ChallengeManager.Add(
+                LobotomyPlugin.pluginGuid,
+                TITLE,
+                DESCRIPTION,
+                20,
+                TextureLoader.LoadTextureFromFile("ascensionLiarStart.png"),
+                TextureLoader.LoadTextureFromFile("ascensionLiarStart_activated.png"), 0)
+                .Challenge.challengeType;
+        }
+    }
+}
