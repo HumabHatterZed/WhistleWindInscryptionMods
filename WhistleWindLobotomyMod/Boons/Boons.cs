954cfb6c68d4707f3d6ab3bfe886a8f352f550cd
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-03-25 16:39:41 -0700

    Overhaul Staining Rose mechanics

--- WhistleWindLobotomyMod/Boons/Boons.cs
+++ WhistleWindLobotomyMod/Boons/Boons.cs
@@ -10,31 +10,31 @@ using WhistleWind.Core.Helpers;
 namespace WhistleWindLobotomyMod {
     public class Boons {
         public static BoonData.Type RoseCurse { get; private set; }
-        public static BoonData.Type RoseCurseWeak { get; private set; }
-        public static BoonData.Type RoseBoon { get; private set; }
+        //public static BoonData.Type RoseCurseWeak { get; private set; }
+        //public static BoonData.Type RoseBoon { get; private set; }
         internal static void Initialise() {
-            Texture2D background = TextureLoader.LoadTextureFromFile("boonRoseBackground.png", LobotomyPlugin.ModAssembly);
             RoseCurse = BoonManager.New<StainingRoseBoon>(LobotomyPlugin.pluginGuid,
-                "Curse of the Rose", "At the end of your turn, all cards on your side of the board gain 1 Paper Rose.",
-                TextureLoader.LoadTextureFromFile("boonRoseIcon1.png", LobotomyPlugin.ModAssembly), background,
+                "Curse of the Staining Rose", "You will start the battle with a Staining Rose in your hand.",
+                TextureLoader.LoadTextureFromFile("boonRoseIcon.png", LobotomyPlugin.ModAssembly),
+                TextureLoader.LoadTextureFromFile("boonRoseBackground.png", LobotomyPlugin.ModAssembly),
                 false, true);
 
-            RoseCurseWeak = BoonManager.New<StainingRoseBoon>(LobotomyPlugin.pluginGuid,
-                "Thirst of the Rose", "At the end of your turn, all cards on your side of the board without Paper Rose gain 1 Paper Rose.",
-                TextureLoader.LoadTextureFromFile("boonRoseIcon2.png", LobotomyPlugin.ModAssembly), background,
-                false, true);
+            //RoseCurseWeak = BoonManager.New<StainingRoseBoon>(LobotomyPlugin.pluginGuid,
+            //    "Thirst of the Rose", "At the end of your turn, all cards on your side of the board without Paper Rose gain 1 Paper Rose.",
+            //    TextureLoader.LoadTextureFromFile("boonRoseIcon2.png", LobotomyPlugin.ModAssembly), background,
+            //    false, true);
 
-            RoseBoon = BoonManager.New<StainingRoseBoon>(LobotomyPlugin.pluginGuid,
-                "Boon of the Rose", "When the scales are tipped towards you, gain 1 life at the start of your turn.",
-                TextureLoader.LoadTextureFromFile("boonRoseIcon3.png", LobotomyPlugin.ModAssembly), background,
-                false, true);
+            //RoseBoon = BoonManager.New<StainingRoseBoon>(LobotomyPlugin.pluginGuid,
+            //    "Boon of the Rose", "When the scales are tipped towards you, gain 1 life at the start of your turn.",
+            //    TextureLoader.LoadTextureFromFile("boonRoseIcon3.png", LobotomyPlugin.ModAssembly), background,
+            //    false, true);
 
-            BoonManager.AllBoonsCopy.Find(x => x.type == RoseCurse)
-                .SetAbilityRedirect("Paper Rose", Ability.None, Color.red);
+            //BoonManager.AllBoonsCopy.Find(x => x.type == RoseCurse)
+            //    .SetAbilityRedirect("Paper Rose", Ability.None, Color.red);
 
 
-            BoonManager.AllBoonsCopy.Find(x => x.type == RoseCurseWeak)
-                .SetAbilityRedirect("Paper Rose", Ability.None, Color.red);
+            //BoonManager.AllBoonsCopy.Find(x => x.type == RoseCurseWeak)
+            //    .SetAbilityRedirect("Paper Rose", Ability.None, Color.red);
         }
     }
 }
