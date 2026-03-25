49bd6d59e210e39da0908d3396d41e73daa38618
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-03-25 16:42:08 -0700

    Add Rose boon at start of run

--- WhistleWindLobotomyMod/Challenges/StartingRose.cs
+++ WhistleWindLobotomyMod/Challenges/StartingRose.cs
@@ -1,16 +1,19 @@
 ﻿using DiskCardGame;
+using HarmonyLib;
 using InscryptionAPI.Ascension;
+using System.Collections;
 using System.Linq;
 using WhistleWind.Core.Helpers;
+using WhistleWindLobotomyMod.Core;
 
 namespace WhistleWindLobotomyMod.Challenges {
     public static class StartingRose {
         internal const string TITLE = "Painted Red";
-        internal const string DESCRIPTION = "You have the Curse of the Rose in all battles.";
+        internal const string DESCRIPTION = "You start the run with the Curse of the Staining Rose.";
 
         public static AscensionChallenge ID { get; private set; }
 
-        internal static void Register() {
+        internal static void Register(Harmony harmonyInstance) {
             ID = ChallengeManager.Add(
                 LobotomyPlugin.pluginGuid,
                 TITLE,
@@ -19,6 +22,16 @@ namespace WhistleWindLobotomyMod.Challenges {
                 TextureLoader.LoadTextureFromFile("ascensionRose.png"),
                 TextureLoader.LoadTextureFromFile("ascensionRose_activated.png"), 0)
                 .Challenge.challengeType;
+
+            harmonyInstance.PatchAll(typeof(StartingRose));
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(RunIntroSequencer), nameof(RunIntroSequencer.RunIntroSequence))]
+        private static IEnumerator AddStainingRoseBoon(IEnumerator result) {
+            if (LobotomyConfigManager.ChallengeIsActive(StartingRose.ID)) {
+                RunState.Run.playerDeck.AddBoon(Boons.RoseCurse);
+            }
+            yield return result;
         }
     }
 }
