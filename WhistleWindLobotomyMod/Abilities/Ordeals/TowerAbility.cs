9e2a375f117b71329a1238b34a4ca04d0e224d7c
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-03-25 20:56:42 -0700

    Fix redirect

--- WhistleWindLobotomyMod/Abilities/Ordeals/TowerAbility.cs
+++ WhistleWindLobotomyMod/Abilities/Ordeals/TowerAbility.cs
@@ -13,9 +13,10 @@ namespace WhistleWindLobotomyMod {
             info.rulebookDescription = "While this card is in an active state, the Light of the End will target spaces on the player's side of the board.";
             info.powerLevel = 5;
             info.passive = true;
-            info.SetUniqueRedirect("Light of the End", "wstl:Ordeals_Light of the End", Color.green);
 
             TowerAbility.ID = AbilityManager.Add(LobotomyPlugin.pluginGuid, info, null, TextureLoader.LoadTextureFromFile("sigilTower.png")).Id;
+            // cannot add redirects until after the info's been added to the API
+            info.SetUniqueRedirect("Light of the End", "wstl:Ordeals_Light of the End", Color.green);
         }
     }
 
