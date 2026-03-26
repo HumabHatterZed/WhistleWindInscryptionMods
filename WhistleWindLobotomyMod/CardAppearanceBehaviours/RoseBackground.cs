47bc5428c469d4c5575a7f7827718c3abdd6f8cc
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-03-25 19:53:59 -0700

    Add Staining Rose bg

---
+++ WhistleWindLobotomyMod/CardAppearanceBehaviours/RoseBackground.cs
@@ -0,0 +1,18 @@
+﻿using DiskCardGame;
+using InscryptionAPI.PixelCard;
+using UnityEngine;
+using WhistleWind.Core.Helpers;
+
+
+namespace WhistleWindLobotomyMod {
+    public partial class Appearances {
+        private static void AddRoseBackground() {
+            RoseBackground.appearance = CardHelper.CreateAppearance<RoseBackground>(LobotomyPlugin.pluginGuid, "RoseBackground").Id;
+        }
+    }
+    public class RoseBackground : CardAppearanceBehaviour {
+        public static Appearance appearance;
+        private static Texture2D bg = null;
+        public override void ApplyAppearance() => base.Card.RenderInfo.baseTextureOverride = (bg ??= TextureLoader.LoadTextureFromFile("roseBackground.png"));
+    }
+}
