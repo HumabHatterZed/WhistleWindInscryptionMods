a72076d0797fa3bb01563c9a69fdd7cb3e6b3601
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-07-31 21:03:31 -0700

    Change to apply panics

--- WhistleWind.AbnormalSigils/Abilities/Status Effects/Sinking.cs
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/Sinking.cs
@@ -1,32 +1,51 @@
 ﻿using DiskCardGame;
+using EasyFeedback.APIs;
+using InscryptionAPI.Card;
 using InscryptionAPI.Triggers;
+using System.Collections;
+using System.Collections.Generic;
 using WhistleWind.AbnormalSigils.StatusEffects;
 using WhistleWind.Core.Helpers;
 
 namespace WhistleWind.AbnormalSigils {
     /// <summary>
-    /// A card bearing this effect loses Power equal to its Sinking divided by its max Health, rounded down.
-    /// </summary>
-    public class Sinking : StatusEffectBehaviour, IPassiveAttackBuff {
+    /// When a card bearing this effect has 5 or more Sinking, enter a Panicked state based on its play cost: Blood: Murder; Bone: Suicide; Energy: Languish; Mox: Defile.
+    public class Sinking : StatusEffectBehaviour {
+        public const int MAX_POTENCY = 5;
+        
         public static Ability iconId;
         public static SpecialTriggeredAbility specialAbility;
         public override Ability IconAbility => iconId;
         public override SpecialTriggeredAbility StatusEffect => specialAbility;
 
-        public int GetPassiveAttackBuff(PlayableCard target) {
-            if (target == base.PlayableCard) {
-                int powerDown = this.EffectPotency / target.MaxHealth;
-                if (powerDown > 0) {
-                    return -powerDown;
-                }
+        public override int MaxPotency => MAX_POTENCY;
+
+        public override bool RespondsToStatusEffectAdded(PlayableCard target, int amount, StatusEffectBehaviour statusEffect, bool alreadyHasStatus) {
+            return target == base.PlayableCard && statusEffect.IconAbility == this.IconAbility;
+        }
+        public override IEnumerator OnStatusEffectAdded(PlayableCard target, int amount, StatusEffectBehaviour statusEffect, bool alreadyHasStatus) {
+            // apply panic
+            List<SpecialTriggeredAbility> possiblePanics = new(4);
+            if (target.BloodCost() > 0) {
+                possiblePanics.Add(PanicBlood.specialAbility);
             }
-            return 0;
+            if (target.BonesCost() > 0) {
+                possiblePanics.Add(PanicBones.specialAbility);
+            }
+            if (target.EnergyCost > 0) {
+                possiblePanics.Add(PanicEnergy.specialAbility);
+            }
+            if (target.GemsCost().Count > 0) {
+                possiblePanics.Add(PanicMox.specialAbility);
+            }
+            yield return target.AddStatusEffect(possiblePanics[SeededRandom.Range(0, possiblePanics.Count, base.GetRandomSeed())], 1);
+            yield return DialogueHelper.PlayDialogueEvent("LearnPanic");
         }
     }
     public partial class AbnormalPlugin {
         private void StatusEffect_Sinking() {
             const string rName = "Sinking";
-            const string rDesc = "A card bearing this effect loses Power equal to its Sinking divided by its max Health, rounded down.";
+            const string rDesc = "When a card has 5 or more Sinking, it enters a Panicked state based on its play cost: Blood: Murderous; Bone: Suicidal; Energy: Languishing; Mox: Traitorous.";
             StatusEffectManager.FullStatusEffect data = StatusEffectManager.New<Sinking>(
                 pluginGuid, rName, rDesc, -1, GameColors.Instance.glowSeafoam,
                 TextureLoader.LoadTextureFromFile("sigilSinking.png", Assembly),
