764f0e51fd55f270dcc484afd96682fbf44915c2
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-08-02 15:53:05 -0700

    Implement Murderous

--- WhistleWind.AbnormalSigils/Abilities/Status Effects/PanicBlood.cs
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/PanicBlood.cs
@@ -1,6 +1,9 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Card;
+using InscryptionAPI.Helpers.Extensions;
+using InscryptionAPI.Triggers;
 using System.Collections;
+using System.Collections.Generic;
 using WhistleWind.AbnormalSigils.Core;
 using WhistleWind.AbnormalSigils.StatusEffects;
 using WhistleWind.Core.Helpers;
@@ -11,12 +14,22 @@ namespace WhistleWind.AbnormalSigils {
         public static SpecialTriggeredAbility specialAbility;
         public override Ability IconAbility => iconId;
         public override SpecialTriggeredAbility StatusEffect => specialAbility;
+
+        public override List<CardSlot> CollectModifyAttackSlots(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, ref int attackCount, ref bool didRemoveDefaultSlot) {
+            int seed = base.GetRandomSeed();
+            List<CardSlot> slots = new();
+            List<CardSlot> adjacentSlots = Singleton<BoardManager>.Instance.GetAdjacentSlots(card.Slot);
+            for (int i = 0; i < attackCount; i++) {
+                slots.Add(adjacentSlots.GetSeededRandom(seed++));
+            }
+            return slots;
+        }
     }
 
     public partial class AbnormalPlugin {
         private void Panic_Blood() {
             const string rName = "Murderous";
-            const string rDesc = "A card bearing this effect loses Speed equal to its Bind. At the end of the owner's turn, remove this effect.";
+            const string rDesc = "A card in this state will strike adjacent spaces during combat. At the end of its owner's turn, remove all Sinking from this card and stop Panicking.";
             StatusEffectManager.FullStatusEffect data = StatusEffectManager.New<PanicBlood>(
                 pluginGuid, rName, rDesc, -3, GameColors.Instance.glowRed,
                 TextureLoader.LoadTextureFromFile("sigilBind.png", Assembly),
