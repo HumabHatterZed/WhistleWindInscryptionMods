466911389af3eaf620928347196839cfb269208b
WhistleWind <69230920+HumabHatterZed@users.noreply.github.com>
2026-08-02 15:52:38 -0700

    Complete PanicBase logic

--- WhistleWind.AbnormalSigils/Abilities/Status Effects/PanicBase.cs
+++ WhistleWind.AbnormalSigils/Abilities/Status Effects/PanicBase.cs
@@ -1,12 +1,15 @@
 ﻿using DiskCardGame;
 using InscryptionAPI.Card;
+using InscryptionAPI.Triggers;
 using System.Collections;
+using System.Collections.Generic;
 using WhistleWind.AbnormalSigils.Core;
 using WhistleWind.AbnormalSigils.StatusEffects;
 using WhistleWind.Core.Helpers;
 
 namespace WhistleWind.AbnormalSigils {
-    public abstract class PanicBase : ModifyOnTurnEndStatusEffectBehaviour {
+    public abstract class PanicBase : ModifyOnTurnEndStatusEffectBehaviour, ISetupAttackSequence {
+        public override int MaxPotency => 1;
         public override int PotencyModification => -EffectPotency;
 
         public override bool RespondsToStatusEffectRemoved(PlayableCard target, StatusEffectBehaviour statusEffect) {
@@ -15,6 +18,16 @@ namespace WhistleWind.AbnormalSigils {
         public override IEnumerator OnStatusEffectRemoved(PlayableCard target, StatusEffectBehaviour statusEffect) {
             // remove all Sinking from this card when ending the panic
             yield return target.RemoveStatusEffect<Sinking>();
+        }
+
+        public abstract List<CardSlot> CollectModifyAttackSlots(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, ref int attackCount, ref bool didRemoveDefaultSlot);
+
+        public bool RespondsToModifyAttackSlots(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, int attackCount, bool didRemoveDefaultSlot) {
+            return card == base.Card && modType == OpposingSlotTriggerPriority.PostAdditionModification;
+        }
+
+        public int GetTriggerPriority(PlayableCard card, OpposingSlotTriggerPriority modType, List<CardSlot> originalSlots, List<CardSlot> currentSlots, int attackCount, bool didRemoveDefaultSlot) {
+            return 0;
         }
 
         public static bool IsPanicking(PlayableCard card) {
